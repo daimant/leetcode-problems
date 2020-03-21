b7c503e181d2de9a635e0f3395d485c7917c7054
Sergey Pomortsev <zaooooza@gmail.com>
2020-03-21 19:02:28 +0300

    add 1385 problem

---
+++ leetcode-tasks/1385-find-the-distance-value-between-two-arrays.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @param {number} d
+ * @return {number}
+ */
+var findTheDistanceValue = function(arr1, arr2, d) {
+  let count = 0;
+
+  for (let i = 0; i < arr1.length; i++) {
+    let currCheck = true;
+
+    for (let j = 0; j < arr2.length; j++) {
+      if (Math.abs(arr1[i] - arr2[j]) <= d) currCheck = false;
+    }
+
+    if (currCheck === true) count++;
+  }
+
+  return count;
+};
