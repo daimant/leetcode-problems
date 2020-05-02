01663ca4a38898c2c061aec8db2b5b0924202041
Sergey Pomortsev <zaooooza@gmail.com>
2020-05-02 21:31:21 +0300

    added 1431 problem

---
+++ leetcode-tasks/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+  const max = Math.max(...candies);
+  return candies.map(el => el + extraCandies >= max);
+};
