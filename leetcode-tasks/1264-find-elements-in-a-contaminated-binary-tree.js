cdd2b8d3382d75576deeebe8c7e2f1bb130e87bf
Sergey Pomortsev <zaooooza@gmail.com>
2020-07-01 14:16:23 +0300

    added 1264 problem

---
+++ leetcode-tasks/1264-find-elements-in-a-contaminated-binary-tree.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+const FindElements = function(root) {
+  this.change = (tree, curr = 0) => {
+    if (!tree) return;
+
+    tree.val = curr;
+    this.unique.add(curr);
+
+    if (tree.left) this.change(tree.left, curr * 2 + 1);
+    if (tree.right) this.change(tree.right, curr * 2 + 2);
+  };
+  this.unique = new Set();
+
+  this.change(root);
+  // console.log(JSON.stringify(root));
+};
+
+/**
+ * @param {number} target
+ * @return {boolean}
+ */
+FindElements.prototype.find = function(target) {
+  return this.unique.has(target);
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * var obj = new FindElements(root)
+ * var param_1 = obj.find(target)
+ */
