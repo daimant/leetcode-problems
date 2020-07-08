1622cf0e7ba61ff43265fbf5a36aa1c4d5c78cfb
Sergey Pomortsev <zaooooza@gmail.com>
2020-07-08 21:17:29 +0300

    added 463 problem

---
+++ leetcode-tasks/0463-island-perimeter.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const islandPerimeter = function(grid) {
+  const xLen = grid.length;
+  const yLen = grid[0].length;
+  let count = 0;
+
+  for (let x = 0; x < xLen; x++) {
+    for (let y = 0; y < yLen; y++) {
+      if (grid[x][y] && (!grid[x - 1] || (x > 0 && !grid[x - 1][y]))) count++;
+      if (grid[x][y] && (!grid[x + 1] || (x < xLen - 1 && !grid[x + 1][y])))
+        count++;
+      if (grid[x][y] && (!grid[x][y - 1] || (y > 0 && !grid[x][y - 1])))
+        count++;
+      if (grid[x][y] && (!grid[x][y + 1] || (y < yLen - 1 && !grid[x][y + 1])))
+        count++;
+    }
+  }
+
+  return count;
+};
