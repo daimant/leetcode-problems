920bf29b96e2f3d536cc9b17d2218b1a6590f045
Sergey Pomortsev <zaooooza@gmail.com>
2020-11-18 15:55:58 +0300

    add 1640 problem

---
+++ leetcode-tasks/1640-check-array-formation-through-concatenation.js
@@ -0,0 +1,89 @@
+//First version
+/**
+ * @param {number[]} arr
+ * @param {number[][]} pieces
+ * @return {boolean}
+ */
+const canFormArray = function(arr, pieces) {
+  for (let i = 0; i < arr.length; i++) {
+    let check = [];
+
+    for (let j = 0; j < pieces.length; j++) {
+      // console.log('i:',i,' j:', j)
+      if (pieces[j][0] === arr[i]) {
+        check = [...pieces[j]];
+        // console.log('choise arr:', check)
+        break;
+      }
+
+      if (pieces[j][0] !== arr[i] && j === pieces.length - 1) return false;
+    }
+
+    for (let k = 1; k < check.length; k++) {
+      i++;
+      // console.log('i:', i,' k:', k, arr[i],check[k])
+      if (arr[i] === check[k]) continue;
+      else return false;
+    }
+  }
+
+  return true;
+};
+
+//second version
+/**
+ * @param {number[]} arr
+ * @param {number[][]} pieces
+ * @return {boolean}
+ */
+const canFormArray2 = function(arr, pieces) {
+  const checked = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    let check = [];
+
+    for (let j = 0; j < pieces.length; j++) {
+      if (checked.has(j)) continue;
+      // console.log('i:',i,' j:', j)
+      if (pieces[j][0] === arr[i]) {
+        check = [...pieces[j]];
+        checked.add(j);
+        // console.log('choise arr:', check)
+        break;
+      }
+
+      if (pieces[j][0] !== arr[i] && j === pieces.length - 1) return false;
+    }
+
+    for (let k = 1; k < check.length; k++) {
+      i++;
+      // console.log('i:', i,' k:', k, arr[i],check[k])
+      if (arr[i] !== check[k]) return false;
+    }
+  }
+
+  return true;
+};
+
+//third optimized solution
+/**
+ * @param {number[]} arr
+ * @param {number[][]} pieces
+ * @return {boolean}
+ */
+const canFormArray3 = function(arr, pieces) {
+  const piecesMap = new Map(pieces.map((el, i) => [(el = el[0]), i]));
+
+  for (let i = 0; i < arr.length; i++) {
+    let check = [];
+
+    if (!piecesMap.has(arr[i])) return false;
+    else check = [...pieces[piecesMap.get(arr[i])]];
+
+    for (let k = 1; k < check.length; k++) {
+      i++;
+      if (arr[i] !== check[k]) return false;
+    }
+  }
+
+  return true;
+};
