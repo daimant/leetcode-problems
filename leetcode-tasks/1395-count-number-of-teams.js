4293bca549f309ad767b0173c44b7c43aa0565c5
Sergey Pomortsev <zaooooza@gmail.com>
2021-01-28 12:05:24 +0300

    add 1395 problem

---
+++ leetcode-tasks/1395-count-number-of-teams.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} rating
+ * @return {number}
+ */
+const numTeams = function(rating) {
+  const { length: len } = rating;
+  let result = 0;
+
+  for (let i = 0; i < len - 2; i++) {
+    for (let j = i + 1; j < len - 1; j++) {
+      for (let k = j + 1; k < len; k++) {
+        if (
+          (rating[i] < rating[j] && rating[j] < rating[k]) ||
+          (rating[i] > rating[j] && rating[j] > rating[k])
+        )
+          result++;
+      }
+    }
+  }
+
+  return result;
+};
