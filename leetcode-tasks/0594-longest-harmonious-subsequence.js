ed85dadc538a1fc7c5fe36ca8b6d72b4a61b02c2
Sergey Pomortsev <zaooooza@gmail.com>
2021-02-06 18:37:46 +0300

    update problem 594

--- leetcode-tasks/0594-longest-harmonious-subsequence.js
+++ leetcode-tasks/0594-longest-harmonious-subsequence.js
@@ -1,21 +1,20 @@
-"use strict";
-
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var findLHS = function(nums) {
+const findLHS = function(nums) {
   let maxLength = 0;
-  let map = new Map();
+  const map = new Map();
 
   for (let i = 0; i < nums.length; i++) {
     if (map.has(nums[i])) map.set(nums[i], map.get(nums[i]) + 1);
     if (!map.has(nums[i])) map.set(nums[i], 1);
   }
 
-  for (let i = 0; i < nums.length - 1; i++) {
-    if (map.get(nums[i]) + map.get(nums[i] + 1) > maxLength)
-      maxLength = map.get(nums[i]) + map.get(nums[i] + 1);
+  for (let i of map) {
+    if (map.get(i[0] + 1) + i[1] > maxLength) {
+      maxLength = map.get(i[0] + 1) + i[1];
+    }
   }
 
   return maxLength;
