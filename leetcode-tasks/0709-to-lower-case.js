0a1470a33b29b49ab22d2b80f06dca04d89ecd39
Sergey Pomortsev <zaooooza@gmail.com>
2021-05-24 11:48:41 +0300

    update problem 709 (easy)

--- leetcode-tasks/0709-to-lower-case.js
+++ leetcode-tasks/0709-to-lower-case.js
@@ -1,9 +1,5 @@
-"use strict";
-
 /**
- * @param {string} str
+ * @param {string} s
  * @return {string}
  */
-var toLowerCase = function(str) {
-  return str.toLowerCase();
-};
+const toLowerCase = s => s.split('').map(el => el.charCodeAt() < 91 && el.charCodeAt() > 64 ? String.fromCharCode(el.charCodeAt() + 32) : el).join('');
