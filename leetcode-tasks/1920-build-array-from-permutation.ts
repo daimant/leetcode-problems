eb3f10aef51aca8980dd00c275ae0dd5b034bde1
Sergey Pomortsev <zaooooza@gmail.com>
2022-03-08 15:53:57 +0300

    solved problem 1920 (easy)

---
+++ leetcode-tasks/1920-build-array-from-permutation.ts
@@ -0,0 +1,4 @@
+const buildArray = (nums: number[]): number[] => nums.map((el, i, arr) => arr[el])
+
+// Runtime: 100 ms, faster than 72.64% of TypeScript online submissions for Build Array from Permutation.
+// Memory Usage: 46.2 MB, less than 66.98% of TypeScript online submissions for Build Array from Permutation.
