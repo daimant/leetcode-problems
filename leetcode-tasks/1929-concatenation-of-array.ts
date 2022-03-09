8a9fffe390d5c40ad19190d1f7ea239948f72546
Sergey Pomortsev <zaooooza@gmail.com>
2022-03-09 11:55:37 +0300

    solved problem 1929 (easy)

---
+++ leetcode-tasks/1929-concatenation-of-array.ts
@@ -0,0 +1,4 @@
+const getConcatenation = (nums: number[]): number[] => [...nums, ...nums]
+
+// Runtime: 84 ms, faster than 93.44% of TypeScript online submissions for Concatenation of Array.
+// Memory Usage: 46.9 MB, less than 28.13% of TypeScript online submissions for Concatenation of Array.
