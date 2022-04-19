9658a472fbe2d39e7698255ed76084ac69c5fb0e
Sergey Pomortsev <zaooooza@gmail.com>
2022-04-19 15:02:13 +0300

    solved problem 2235 (easy)

---
+++ leetcode-tasks/2235-add-two-integers.ts
@@ -0,0 +1,4 @@
+const sum = (num1: number, num2: number): number => num1 + num2
+
+// Runtime: 70 ms, faster than 65.52% of TypeScript online submissions for Add Two Integers.
+// Memory Usage: 44.5 MB, less than 6.90% of TypeScript online submissions for Add Two Integers.
