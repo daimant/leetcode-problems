00924e5be6a38898f917d48681c5cbb92b831a23
Sergey Pomortsev <zaooooza@gmail.com>
2022-11-20 10:10:19 +0300

    added new solution for problem 2465 (easy)

--- leetcode-tasks/2465-number-of-distinct-averages.ts
+++ leetcode-tasks/2465-number-of-distinct-averages.ts
@@ -1,3 +1,4 @@
+// elegant solution Runtime: 79 ms Memory Usage: 42.5 MB
 const distinctAverages = (nums: number[]): number => {
   const set = new Set()
 
@@ -6,6 +7,21 @@ const distinctAverages = (nums: number[]): number => {
       set.add((el + nums[nums.length - 1 - i]) / 2)
     }
   })
+
+  return set.size
+}
+
+// fastest solution
+// Runtime: 64 ms, faster than 100.00% of TypeScript online submissions for Number of Distinct Averages.
+// Memory Usage: 43.1 MB, less than 57.14% of TypeScript online submissions for Number of Distinct Averages.
+const distinctAverages2 = (nums: number[]): number => {
+  const set = new Set()
+
+  nums.sort((a, b) => a - b)
+
+  for (let i = 0; i < nums.length / 2; i++) {
+    set.add((nums[i] + nums[nums.length - 1 - i]) / 2)
+  }
 
   return set.size
 }
