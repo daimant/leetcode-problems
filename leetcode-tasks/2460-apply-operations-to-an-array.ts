c48ab3f5c7b3dba5f8ee12b342eae3f7c12bf14e
Sergey Pomortsev <zaooooza@gmail.com>
2022-11-22 12:41:29 +0300

    solved problem 2460 (easy)

---
+++ leetcode-tasks/2460-apply-operations-to-an-array.ts
@@ -0,0 +1,16 @@
+// Runtime: 90 ms, faster than 72.13% of TypeScript online submissions for Apply Operations to an Array.
+// Memory Usage: 44.3 MB, less than 98.36% of TypeScript online submissions for Apply Operations to an Array.
+
+const applyOperations = (nums: number[]): number[] => {
+  const startLength = nums.length
+
+  nums = nums.map((el, i, arr) => {
+    if (el === arr[i + 1]) {
+      el += arr[i + 1]
+      arr[i + 1] = 0
+    }
+    return el
+  }).filter(el => el)
+
+  return nums.concat(new Array(startLength - nums.length).fill(0))
+}
