55d1151b85675b9a29cf3cd98b08ff543c041152
Sergey Pomortsev <zaooooza@gmail.com>
2022-11-23 09:08:27 +0300

    added additional solution problem 2455 (easy)

--- leetcode-tasks/2455-average-value-of-even-numbers-that-are-divisible-by-three.ts
+++ leetcode-tasks/2455-average-value-of-even-numbers-that-are-divisible-by-three.ts
@@ -7,3 +7,17 @@ const averageValue = (nums: number[], countEven = 0): number => Math.trunc(nums.
     return acc + curr
   } else return acc
 }, 0) / countEven || 0)
+
+// Runtime: 72 ms, faster than 88.89% of TypeScript online submissions for Average Value of Even Numbers That Are Divisible by Three.
+// Memory Usage: 45 MB, less than 49.21% of TypeScript online submissions for Average Value of Even Numbers That Are Divisible by Three.
+
+const averageValue2 = (nums: number[]): number => {
+  let countEven = 0
+
+  return Math.trunc(nums.reduce((acc, curr) => {
+    if (!(curr % 2) && !(curr % 3)) {
+      countEven++
+      return acc + curr
+    } else return acc
+  }, 0) / countEven) || 0
+}
