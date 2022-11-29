c8ef225992f4862766a649e98d996f697b75dc8f
Sergey Pomortsev <zaooooza@gmail.com>
2022-11-29 20:05:58 +0300

    added solution for problem 2437 (easy)

--- leetcode-tasks/2437-number-of-valid-clock-times.ts
+++ leetcode-tasks/2437-number-of-valid-clock-times.ts
@@ -6,14 +6,28 @@ const countTime = (time: string): number => {
 
   if (time[0] === '?' && time[1] === '?') {
     result *= 24
-  }
-  else {
+  } else {
     if (time[0] === '?') {
       result *= Number(time[1]) < 4 ? 3 : 2
     }
     if (time[1] === '?') {
       result *= Number(time[0]) !== 2 ? 10 : 4
     }
+  }
+
+  if (time[3] === '?') result *= 6
+  if (time[4] === '?') result *= 10
+
+  return result
+}
+
+const countTime2 = (time: string): number => {
+  let result = 1
+
+  if (time[0] === '?' && time[1] === '?') result *= 24
+  else {
+    if (time[0] === '?') result *= Number(time[1]) < 4 ? 3 : 2
+    else if (time[1] === '?') result *= Number(time[0]) !== 2 ? 10 : 4
   }
 
   if (time[3] === '?') result *= 6
