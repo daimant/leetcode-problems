a352e9fd935055272d18f21b62824697c7a05c3d
Sergey Pomortsev <zaooooza@gmail.com>
2022-12-13 09:09:08 +0300

    added solution for problem 2490 (easy)

--- leetcode-tasks/2490-circular-sentence.ts
+++ leetcode-tasks/2490-circular-sentence.ts
@@ -7,3 +7,13 @@ const isCircularSentence = (s: string): boolean => {
 
   return true
 }
+
+// Runtime 76 ms Beats 84.78% Memory 44.3 MB Beats 84.78%
+
+const isCircularSentence2 = (s: string): boolean => {
+  for (let i = 1; i < s.length - 1; i++) {
+    if (s[i] === ' ' && s[i - 1] !== s[i + 1]) return false
+  }
+
+  return s[0] === s[s.length - 1]
+}
