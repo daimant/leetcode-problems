ce2449c07f0f180f84a3c046c09a047b864c9ac7
Sergey Pomortsev <zaooooza@gmail.com>
2022-12-15 08:41:54 +0300

    added extended solution for problem 2500 (easy)

--- leetcode-tasks/2500-delete-greatest-value-in-each-row.ts
+++ leetcode-tasks/2500-delete-greatest-value-in-each-row.ts
@@ -1,4 +1,5 @@
 // Runtime 75 ms Beats 100% Memory 46 MB Beats 100%
+// mutable
 
 const deleteGreatestValue = (grid: number[][]): number => {
   let result = 0
@@ -13,6 +14,27 @@ const deleteGreatestValue = (grid: number[][]): number => {
     }
 
     result += Math.max(...currMaxValues)
+  }
+
+  return result
+}
+
+// Runtime 70 ms Beats 100% Memory 45.2 MB Beats 68.97%
+// immutable
+
+const deleteGreatestValue2 = (grid: number[][]): number => {
+  let result = 0
+
+  grid.forEach(el => el.sort((a, b) => a - b))
+
+  for (let j = 0; j < grid[0].length; j++) {
+    let currMaxValue = 0
+
+    for (let i = 0; i < grid.length; i++) {
+      if (currMaxValue < grid[i][j]) currMaxValue = grid[i][j]
+    }
+
+    result += currMaxValue
   }
 
   return result
