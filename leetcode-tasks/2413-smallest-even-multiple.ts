316b8c70c3a61786a885e0ad59a04e02a1beeff0
Sergey Pomortsev <zaooooza@gmail.com>
2022-12-22 20:32:01 +0300

    update time information for problem 2413 (easy)

--- leetcode-tasks/2413-smallest-even-multiple.ts
+++ leetcode-tasks/2413-smallest-even-multiple.ts
@@ -1,3 +1,3 @@
-// Runtime 81 ms Beats 70.77% Memory 43.6 MB Beats 5.38%
+// Runtime 70 ms Beats 86.26% Memory 43.5 MB Beats 11.45%
 
 const smallestEvenMultiple = (n: number): number => n % 2 ? n * 2 : n
