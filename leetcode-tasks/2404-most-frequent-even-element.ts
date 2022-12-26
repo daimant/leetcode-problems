4a57ea5e8ebe40382dba6a3bf27c1b4af1d3fb92
Sergey Pomortsev <zaooooza@gmail.com>
2022-12-26 08:44:24 +0300

    added second solution for problem 2404 (easy)

--- leetcode-tasks/2404-most-frequent-even-element.ts
+++ leetcode-tasks/2404-most-frequent-even-element.ts
@@ -28,3 +28,20 @@ const mostFrequentEven2 = (nums: number[]): number => {
 
   return result.length === 0 ? -1 : result.filter(el => el[1] === maxRep).sort((a, b) => a[0] - b[0])[0][0]
 }
+
+// Runtime 85 ms Beats 100% Memory 50.7 MB Beats 73.8%
+
+const mostFrequentEven3 = (nums: number[]): number => {
+  const map: Map<number, number> = new Map()
+
+  nums.forEach(el => {
+    if (el % 2 === 0) map.set(el, map.has(el) ? map.get(el) + 1 : 1)
+  })
+
+  if (!map.size) return -1
+
+  let result = Array.from(map)
+  const maxRep = Math.max(...result.map(el => el[1]))
+
+  return result.filter(el => el[1] === maxRep).sort((a, b) => a[0] - b[0])[0][0]
+}
