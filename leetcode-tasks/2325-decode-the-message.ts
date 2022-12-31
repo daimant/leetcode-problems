7c7c25c2608bdb55b86057205f2c865d118c3ace
Sergey Pomortsev <zaooooza@gmail.com>
2022-12-31 10:27:51 +0300

    added third solution for problem 2325 (easy)

--- leetcode-tasks/2325-decode-the-message.ts
+++ leetcode-tasks/2325-decode-the-message.ts
@@ -27,3 +27,15 @@ const decodeMessage2 = (key: string, message: string): string => {
 }
 
 // Runtime 67 ms Beats 96.36% Memory 45.4 MB Beats 61.82%
+
+const decodeMessage3 = (key: string, message: string): string => {
+  const map = new Map()
+  let spaces = 0
+
+  for (let i = 0; map.size < 26; i++) {
+    if (key[i] === ' ' || map.has(key[i])) spaces++
+    else map.set(key[i], i - spaces)
+  }
+
+  return message.split('').map((el: string) =>  el === ' ' ? el : String.fromCharCode(97 + map.get(el))).join('')
+}
