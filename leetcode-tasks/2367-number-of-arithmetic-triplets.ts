3a43c1d72f6b500bf6176d820cb4b2685e399f05
Sergey Pomortsev <zaooooza@gmail.com>
2023-01-11 10:26:27 +0300

    added second solution for problem 2367 (easy)

--- leetcode-tasks/2367-number-of-arithmetic-triplets.ts
+++ leetcode-tasks/2367-number-of-arithmetic-triplets.ts
@@ -13,3 +13,21 @@ const arithmeticTriplets = (nums: number[], diff: number): number => {
 
   return result
 }
+
+// Runtime 67 ms Beats 94.3% Memory 43.5 MB Beats 73.13%
+
+const arithmeticTriplets2 = (nums: number[], diff: number): number => {
+  let result = 0
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      if (nums[j] - nums[i] !== diff) continue
+
+      for (let k = j + 1; k < nums.length; k++) {
+        if (nums[k] - nums[j] === diff && nums[j] - nums[i] === diff) result++
+      }
+    }
+  }
+
+  return result
+}
