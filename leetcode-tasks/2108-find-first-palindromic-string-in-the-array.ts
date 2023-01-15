9c748ee199f1ac854394d45b6059ac78e639d7d9
Sergey Pomortsev <zaooooza@gmail.com>
2023-01-15 19:12:14 +0300

    added second wrong solution for problem 2108 (easy)

--- leetcode-tasks/2108-find-first-palindromic-string-in-the-array.ts
+++ leetcode-tasks/2108-find-first-palindromic-string-in-the-array.ts
@@ -10,3 +10,13 @@ const firstPalindrome = (words: string[]): string => {
 
   return ''
 }
+
+// 104 / 266 testcases passed
+
+const firstPalindrome2 = (words: string[]): string => {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length === 1 || words[i].slice(0, words[i].length / 2) === words[i].slice(words[i].length / 2 + 1).split('').reverse().join('')) return words[i]
+  }
+
+  return ''
+}
