9491a7c96e56ca0fb5ba649a071c746bd9b887c6
Sergey Pomortsev <zaooooza@gmail.com>
2023-01-16 11:14:06 +0300

    added third wrong solution for problem 2108 (easy)

--- leetcode-tasks/2108-find-first-palindromic-string-in-the-array.ts
+++ leetcode-tasks/2108-find-first-palindromic-string-in-the-array.ts
@@ -20,3 +20,18 @@ const firstPalindrome2 = (words: string[]): string => {
 
   return ''
 }
+
+// 259 / 266 testcases passed
+
+const firstPalindrome3 = (words: string[]): string => {
+  let result = ''
+
+  words.forEach(word => {
+    if ((word.length === 1 || (word.length === 2 && word[0] === word[1]) || word.slice(0, word.length / 2) === word.slice((word.length / 2) % 2 ? word.length / 2 + 1 : word.length / 2).split('').reverse().join('')) && !result.length) result = word
+  })
+
+  return result
+}
+
+
+// Runtime 71 ms Beats 97.62% Memory 48.8 MB Beats 45.24%
