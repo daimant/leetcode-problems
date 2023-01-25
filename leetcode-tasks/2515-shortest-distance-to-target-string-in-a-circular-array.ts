1457b9b421213ab4562663e21a4ff03a44f4cb7e
Sergey Pomortsev <zaooooza@gmail.com>
2023-01-25 11:17:33 +0300

    added solution for problem 2515 (easy)

---
+++ leetcode-tasks/2515-shortest-distance-to-target-string-in-a-circular-array.ts
@@ -0,0 +1,30 @@
+// Runtime 100 ms Beats 11.36% Memory 46.9 MB Beats 20.45%
+
+const closetTarget = (words: string[], target: string, startIndex: number): number => {
+  let moveRight = -1
+  let moveLeft = -1
+
+  for (let i = startIndex; i < words.length && moveRight < 0; i++) {
+    if (words[i] === target) moveRight = i - startIndex
+
+    if (i === words.length - 1) {
+      for (let j = 0; j < startIndex && moveRight < 0; j++) {
+        if (words[j] === target) moveRight = j + i - startIndex + 1
+      }
+    }
+  }
+
+  for (let i = startIndex; i >= 0 && moveLeft < 0; i--) {
+    if (words[i] === target) moveLeft = startIndex - i
+
+    if (i === 0) {
+      for (let j = words.length - 1; j > startIndex && moveLeft < 0; j--) {
+        if (words[j] === target) moveLeft = startIndex + words.length - j
+      }
+    }
+  }
+
+  if (moveRight < 0 && moveRight < 0) return -1
+
+  return Math.min(moveRight < 0 ? Infinity : moveRight, moveLeft < 0 ? Infinity : moveLeft)
+}
