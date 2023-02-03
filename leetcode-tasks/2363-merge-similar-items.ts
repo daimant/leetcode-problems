7be6639039a9f8397e05eb0e65196fbf3899c603
Sergey Pomortsev <zaooooza@gmail.com>
2023-02-03 10:59:30 +0300

    added fast solution for problem 2363 (easy)

--- leetcode-tasks/2363-merge-similar-items.ts
+++ leetcode-tasks/2363-merge-similar-items.ts
@@ -10,3 +10,15 @@ const mergeSimilarItems = (items1: number[][], items2: number[][]): number[][] =
 
   return Array.from(map).sort((a, b) => a[0] - b[0])
 }
+
+// Runtime 75 ms Beats 100% Memory 50.3 MB Beats 48%
+
+const mergeSimilarItems2 = (items1: number[][], items2: number[][]): number[][] => {
+  const map = new Map(items1 as [number, number][])
+
+  items2.forEach(el => {
+    map.set(el[0], map.has(el[0]) ? map.get(el[0]) + el[1] : el[1])
+  })
+
+  return Array.from(map).sort((a, b) => a[0] - b[0])
+}
