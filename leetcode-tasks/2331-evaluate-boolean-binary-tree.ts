56e1bafc7169dffe65cd08edcf6040c7096aa507
Sergey Pomortsev <zaooooza@gmail.com>
2023-02-15 11:07:23 +0300

    added solution for problem 2331 (easy)

--- leetcode-tasks/2331-evaluate-boolean-binary-tree.ts
+++ leetcode-tasks/2331-evaluate-boolean-binary-tree.ts
@@ -28,3 +28,27 @@ const evaluateTree = (root: TreeNode | null): boolean => {
 
   return evaluateTree(root)
 }
+
+// Runtime 72 ms Beats 92.31% Memory 49.4 MB Beats 35.90%
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+const evaluateTree2 = (root: TreeNode | null): boolean => {
+  if (!root || root.val === 0 || root.val === 1) return Boolean(root && root.val)
+
+  if (root.val === 3) return evaluateTree(root.left) && evaluateTree(root.right)
+
+  if (root.val === 2) return evaluateTree(root.left) || evaluateTree(root.right)
+}
