f43010078ca36b157d7a3904c44556e2da86b70d
Sergey Pomortsev <zaooooza@gmail.com>
2023-02-24 22:41:20 +0300

    added fast solution for problem 2351 (easy)

--- leetcode-tasks/2351-first-letter-to-appear-twice.ts
+++ leetcode-tasks/2351-first-letter-to-appear-twice.ts
@@ -15,3 +15,13 @@ const repeatedCharacter = (s: string): string => {
   return result
 }
 
+// Runtime 60 ms Beats 91.30% Memory 42.5 MB Beats 100%
+
+const repeatedCharacter2 = (s: string): string => {
+  const set = new Set()
+
+  for (const el of s) {
+    if (set.has(el)) return el
+    else set.add(el)
+  }
+}
