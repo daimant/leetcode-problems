1e5e9ff6eecb73883b1a832a84ab791364c3483b
Sergey Pomortsev <zaooooza@gmail.com>
2023-02-26 13:22:12 +0300

    added solution for problem 2309 (easy)

--- leetcode-tasks/2309-greatest-english-letter-in-upper-and-lower-case.ts
+++ leetcode-tasks/2309-greatest-english-letter-in-upper-and-lower-case.ts
@@ -12,3 +12,18 @@ const greatestLetter = (s: string): string => {
 
   return result.toUpperCase()
 }
+
+// Runtime 79 ms Beats 73.68% Memory 44.7 MB Beats 94.74%
+
+const greatestLetter2 = (s: string): string => {
+  const set = new Set()
+  let result = ''
+
+  s.split('').sort().forEach(el => {
+    const curr = el.charCodeAt(0)
+    if ((set.has(curr + 32) || set.has(curr - 32)) && el > result) result = el
+    else set.add(curr)
+  })
+
+  return result.toUpperCase()
+}
