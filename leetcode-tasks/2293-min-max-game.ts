4bd62057d9cdca5163cd2ea610bbd7fd375462a9
Sergey Pomortsev <zaooooza@gmail.com>
2023-03-01 12:43:09 +0300

    added elegant solution for problem 2293 (easy)

--- leetcode-tasks/2293-min-max-game.ts
+++ leetcode-tasks/2293-min-max-game.ts
@@ -11,3 +11,14 @@ const minMaxGame = (nums: number[]): number => {
 
   return minMaxGame(nums)
 }
+
+// refactor
+// Runtime 67 ms Beats 100% Memory 44.8 MB Beats 80%
+
+const minMaxGame2 = (nums: number[], arrLength = nums.length / 2): number => {
+  for (let i = 0; i < arrLength && nums.length > 1; i++) {
+    nums.splice(i, 2, i % 2 ? Math.max(nums[i], nums[i + 1]) : Math.min(nums[i], nums[i + 1]))
+  }
+
+  return nums.length === 1 ? nums[0] : minMaxGame(nums)
+}
