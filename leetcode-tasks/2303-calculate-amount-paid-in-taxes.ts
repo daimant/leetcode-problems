3e44aef637213a05f1ed5a6244a10647f9b48aae
Sergey Pomortsev <zaooooza@gmail.com>
2023-03-03 11:42:57 +0300

    added more short and fast solution for problem 2303 (easy)

--- leetcode-tasks/2303-calculate-amount-paid-in-taxes.ts
+++ leetcode-tasks/2303-calculate-amount-paid-in-taxes.ts
@@ -24,3 +24,20 @@ const calculateTax = (brackets: number[][], income: number): number => {
 
   return result
 }
+
+// more short and fast
+// Runtime  54 ms  Beats  100%  Memory  45.3 MB  Beats  50%
+
+const calculateTax2 = (brackets: number[][], income: number): number => {
+  let result = 0
+
+  brackets.forEach((el, i, arr) => {
+    const currVal = el[0] - (i > 0 ? arr[i - 1][0] : 0)
+    let currIncome = currVal < income ? currVal : income
+    income = currVal < income ? income - currVal : 0
+
+    result += currIncome * el[1] / 100
+  })
+
+  return result
+}
