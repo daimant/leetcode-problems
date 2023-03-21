24d484a05ab8c981fc83c2d178ab2b445a3609ca
Sergey Pomortsev <zaooooza@gmail.com>
2023-03-21 07:31:21 +0300

    added solution for problem 1796 (easy)

--- leetcode-tasks/1796-second-largest-digit-in-a-string.ts
+++ leetcode-tasks/1796-second-largest-digit-in-a-string.ts
@@ -16,3 +16,17 @@ const secondHighest = s => {
 
   return -1;
 };
+
+// Runtime 55 ms Beats 100% Memory 45.9 MB Beats 41.18%
+
+const secondHighest2 = (s: string): number => {
+  const set: Set<number> = new Set()
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] >= '0' && s[i] <= '9') set.add(Number(s[i]))
+  }
+
+  const resultArr = [...set].sort((a, b) => b - a)
+
+  return resultArr[1] || (resultArr[1] === 0 ? resultArr[1] : -1)
+}
