2bde8d5019beff6c35d18a1158b2f5239e9f0981
Sergey Pomortsev <zaooooza@gmail.com>
2023-03-23 08:16:38 +0300

    added solution for problem 721 (mid)

--- leetcode-tasks/721-accounts-merge.ts
+++ leetcode-tasks/721-accounts-merge.ts
@@ -24,3 +24,34 @@ const accountsMerge = (accounts: string[][]): string[][] => {
 
   return [...result].map(el => [el[0].replace(/[0-9]/i, ''), ...new Set(el[1].sort((a, b) => a > b ? 1 : -1))])
 }
+
+// Runtime 213 ms Beats 39.51% Memory 60.3 MB Beats 18.52%
+
+const accountsMerge2 = (accounts: string[][]): string[][] => {
+  const emailOwner = new Map()
+  const resultAccs = new Map()
+
+  accounts.forEach((person, iOut) => {
+    const currCryptName = `${iOut}${person[0]}`
+    const currEmails = person.slice(1)
+    let realCurrName: undefined | string
+
+    currEmails.forEach(email => {
+      if (emailOwner.has(email)) realCurrName = emailOwner.get(email)
+    })
+
+    if (realCurrName) {
+      resultAccs.set(realCurrName, [...resultAccs.get(realCurrName), ...currEmails])
+    } else {
+      resultAccs.set(currCryptName, person.slice(1))
+      currEmails.forEach(email => {
+        emailOwner.set(email, currCryptName)
+      })
+    }
+  })
+
+  const result = [...resultAccs].map(el => [el[0].replace(/[0-9]/g, ''), ...new Set(el[1].sort((a, b) => a > b ? 1 : -1))])
+  const resultEmails = result.flat(1).filter(el => el.includes('@'))
+
+  return resultEmails.length === new Set(resultEmails).size ? result : accountsMerge(result)
+}
