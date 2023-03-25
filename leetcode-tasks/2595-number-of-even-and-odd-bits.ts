7262b0265b2db07af57043e4d7e57fb006c53b57
Sergey Pomortsev <zaooooza@gmail.com>
2023-03-25 11:43:25 +0300

    added optimized solution for problem 2595 (easy)

--- leetcode-tasks/2595-number-of-even-and-odd-bits.ts
+++ leetcode-tasks/2595-number-of-even-and-odd-bits.ts
@@ -11,3 +11,19 @@ const evenOddBit = (n: number): number[] => {
 
   return bins.reduce((acc, curr, i) => curr ? (i % 2 ? [acc[0], acc[1] + 1] : [acc[0] + 1, acc[1]]) : acc, [0, 0])
 }
+
+// Runtime 68 ms Beats 88.24% Memory 45.1 MB Beats 26.47%
+
+const evenOddBit2 = (n: number): number[] => {
+  const result = [0, 0]
+  let bins = []
+
+  while (n >= 1) {
+    const curr = n % 2
+    bins.push(curr)
+    n = (n - curr) / 2
+    if (curr) result[bins.length % 2 ? 0 : 1]++
+  }
+
+  return result
+}
