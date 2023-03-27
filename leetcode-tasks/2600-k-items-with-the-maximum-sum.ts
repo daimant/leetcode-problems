845a26e791b7776df7933354b00b9bcecdb887da
Sergey Pomortsev <zaooooza@gmail.com>
2023-03-27 08:02:29 +0300

    added solution for problem 2600 (easy)

--- leetcode-tasks/2600-k-items-with-the-maximum-sum.ts
+++ leetcode-tasks/2600-k-items-with-the-maximum-sum.ts
@@ -5,3 +5,9 @@ const kItemsWithMaximumSum = (numOnes: number, numZeros: number, numNegOnes: num
   else if (numOnes + numZeros >= k) return numOnes
   else return numOnes - k % (numOnes + numZeros)
 }
+
+// Runtime 72 ms Beats 100% Memory 46.3 MB Beats 100%
+
+const kItemsWithMaximumSum2 = (ones: number, zeros: number, neg: number, k: number): number => k
+  ? [...new Array(ones).fill(1), ...new Array(zeros).fill(0), ...new Array(neg).fill(-1)].slice(0, k).reduce((acc, curr) => acc + curr)
+  : 0
