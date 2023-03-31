cf48fda42c362b06222087eafbd41cf0e7f5659e
Sergey Pomortsev <zaooooza@gmail.com>
2023-03-31 13:43:47 +0300

    added one-line solution for problem 2578 (easy)

--- leetcode-tasks/2578-split-with-minimum-sum.ts
+++ leetcode-tasks/2578-split-with-minimum-sum.ts
@@ -8,3 +8,7 @@ const splitNum = (num: number): number => {
 
   return Number(arr[0]) + Number(arr[1])
 }
+
+// Runtime 57 ms Beats 81.25% Memory 44.7 MB Beats 22.92%
+
+const splitNum2 = (num: number): number => String(num).split('').sort((a, b) => a > b ? 1 : -1).reduce((acc, curr, i) => i % 2 ? [acc[0] + curr, acc[1]] : [acc[0], acc[1] + curr], ['', '']).reduce((acc, curr) => acc + +curr, 0)
