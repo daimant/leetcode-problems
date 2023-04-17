b29c356180e3b3fe4ceb5caeb9cdef3f71349683
Sergey Pomortsev <zaooooza@gmail.com>
2023-04-17 13:43:58 +0500

    added wrong solution for problem 2609 (easy)

--- leetcode-tasks/2609-find-the-longest-balanced-substring-of-a-binary-string.ts
+++ leetcode-tasks/2609-find-the-longest-balanced-substring-of-a-binary-string.ts
@@ -1,6 +1,26 @@
-// Runtime 95 ms Beats 68.18% Memory 46.8 MB Beats 29.55%
+// 1091 / 1848 testcases passed
 
 const findTheLongestBalancedSubstring = (s: string): number => {
+  const seqs = []
+  let currLen = 0
+  let result = 0
+
+  for (let i = 0; i < s.length; i++) {
+    currLen++
+
+    if (s[i] !== s[i + 1]) {
+      if (seqs[seqs.length - 1] === currLen && currLen > result) result = currLen
+      seqs.push(currLen)
+      currLen = 0
+    }
+  }
+
+  return result * 2
+}
+
+// Runtime 95 ms Beats 68.18% Memory 46.8 MB Beats 29.55%
+
+const findTheLongestBalancedSubstring2 = (s: string): number => {
   const seqs = []
   let currLen = 0
   let result = 0
