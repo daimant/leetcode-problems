22128f60a3527951e7a2d83fc3edf26d33900483
Sergey Pomortsev <zaooooza@gmail.com>
2023-05-16 08:00:30 +0300

    added optimized solution for problem 1844 (easy)

--- leetcode-tasks/1844-replace-all-digits-with-characters.ts
+++ leetcode-tasks/1844-replace-all-digits-with-characters.ts
@@ -11,3 +11,10 @@ const replaceDigits2 = (s: string): string => s
   .split('')
   .map((el, i, arr) => i > 0 && !isNaN(+el) ? String.fromCharCode(arr[i - 1].charCodeAt(0) + +el) : el)
   .join('')
+
+// Runtime 46 ms Beats 100% Memory 45.1 MB Beats 9.68%
+
+const replaceDigits3 = (s: string): string => s
+  .split('')
+  .map((el, i, arr) => i % 2 ? String.fromCharCode(arr[i - 1].charCodeAt(0) + +el) : el)
+  .join('')
