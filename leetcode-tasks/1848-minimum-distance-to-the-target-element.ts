1e35726130df650cd04479eee82ef6390fc92e1c
Sergey Pomortsev <zaooooza@gmail.com>
2023-05-18 00:12:09 +0300

    added correct solution for problem 1848 (easy)

--- leetcode-tasks/1848-minimum-distance-to-the-target-element.ts
+++ leetcode-tasks/1848-minimum-distance-to-the-target-element.ts
@@ -5,3 +5,15 @@ const getMinDistance = (nums: number[], target: number, start: number): number =
   nums.forEach((el, i) => el === target && i - start < result && (result = i - start))
   return result
 }
+
+// Runtime 59 ms Beats 86.67% Memory 44.9 MB Beats 20%
+
+const getMinDistance2 = (nums: number[], target: number, start: number): number => {
+  const diffs = new Set()
+
+  nums.forEach((el, i) => {
+    if (el === target) diffs.add(Math.abs(i - start))
+  })
+
+  return Math.min(...Array.from(diffs) as number[])
+}
