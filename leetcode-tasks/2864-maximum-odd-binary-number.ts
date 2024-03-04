ad88158d1a9e9792a42f28fa18c86dc2be50e965
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-04 15:53:56 +0500

    added optimized solution for problem 2864 (easy)

--- leetcode-tasks/2864-maximum-odd-binary-number.ts
+++ leetcode-tasks/2864-maximum-odd-binary-number.ts
@@ -19,3 +19,27 @@ const maximumOddBinaryNumber = (s: string): string => {
 
   return max.join('')
 }
+
+// Accepted
+// Runtime 67 ms Beats 95.02% of users with TypeScript
+// Memory 56.10 MB Beats 5.24% of users with TypeScript
+
+const maximumOddBinaryNumber2 = (s: string): string => {
+  const oneCount = s.split('').filter(el => el === '1').length
+  let max = [...Array(oneCount).fill('1'), ...Array(s.length - oneCount).fill('0')]
+
+  let start = max.indexOf('0')
+  let end = max.lastIndexOf('0') - 1
+
+  while (max[0] !== '0') {
+    if (BigInt(`0b${max.join('')}`) % BigInt('2')) return max.join('')
+
+    start--
+    end++
+
+    max.splice(start, 1, '0')
+    max.splice(end, 1, '1')
+  }
+
+  return max.join('')
+};
