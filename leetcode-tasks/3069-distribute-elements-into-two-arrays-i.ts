82076cac4f8e8fe234f46226fab2d2918d546270
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-06 11:25:40 +0500

    added optimized solution for problem 3069 (easy)

--- leetcode-tasks/3069-distribute-elements-into-two-arrays-i.ts
+++ leetcode-tasks/3069-distribute-elements-into-two-arrays-i.ts
@@ -14,3 +14,19 @@ const resultArray = (nums: number[]): number[] => {
 
   return [...arr1, ...arr2]
 }
+
+// optimized solution
+// Accepted
+// Solution Runtime 57 ms Beats 100.00% of users with TypeScript
+// Memory 53.36 MB Beats 100.00% of users with TypeScript
+
+const resultArray2 = (nums: number[]): number[] => {
+  const arr1 = [nums[0]]
+  const arr2 = [nums[1]]
+
+  for (let i = 2; i < nums.length; i++) {
+    (arr1[arr1.length - 1] > arr2[arr2.length - 1] ? arr1 : arr2).push(nums[i])
+  }
+
+  return [...arr1, ...arr2]
+}
