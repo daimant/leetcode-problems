31f826dc9b67a36e2ba07cbb716b492e4c167d70
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-08 10:59:52 +0500

    added time for problem 141 (easy)

--- leetcode-tasks/0141-linked-list-cycle.js
+++ leetcode-tasks/0141-linked-list-cycle.js
@@ -1,4 +1,6 @@
-"use strict";
+// Accepted
+// Runtime 60 ms Beats 77.55% of users with JavaScript
+// Memory 37.51 MB Beats 100.00% of users with JavaScript
 
 /**
  * Definition for singly-linked list.
