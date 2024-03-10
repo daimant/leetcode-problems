4b4f0827390a7ade33c7d6da9c5f383b63161ffe
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-10 15:53:08 +0500

    added optimized solution for problem 2540 (easy)

--- leetcode-tasks/2540-minimum-common-value.ts
+++ leetcode-tasks/2540-minimum-common-value.ts
@@ -3,8 +3,23 @@
 const getCommon = (nums1: number[], nums2: number[]): number => {
   const set = new Set(nums1)
 
-  for(let i = 0; i < nums2.length; i++) {
+  for (let i = 0; i < nums2.length; i++) {
     if (set.has(nums2[i])) return nums2[i]
+  }
+
+  return -1
+}
+
+// added optimized solution
+// Accepted
+// Runtime 49 ms Beats 98.33% of users with TypeScript
+// Memory 59.44 MB Beats 53.33% of users with TypeScript
+
+const getCommon2 = (nums1: number[], nums2: number[]): number => {
+  for (let i = 0, j = 0; i < nums1.length && j < nums2.length;) {
+    if (nums1[i] < nums2[j]) i++
+    else if (nums1[i] > nums2[j]) j++
+    else return nums1[i]
   }
 
   return -1
