d0f73d5d55daff82bf87b4ad32f93cf02395364f
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-13 10:36:41 +0500

    added solution for problem 3010 (easy) https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i/description/

--- leetcode-tasks/3010-divide-an-array-into-subarrays-with-minimum-cost-i.ts
+++ leetcode-tasks/3010-divide-an-array-into-subarrays-with-minimum-cost-i.ts
@@ -20,3 +20,22 @@ const minimumCost = (nums: number[]): number => {
 
   return a + b + c
 }
+
+// Accepted
+// Runtime 65 ms Beats 95.56% of users with TypeScript
+// Memory 53.25 MB Beats 90.00% of users with TypeScript
+
+const minimumCost2 = (nums: number[]): number => {
+  if (nums.length === 3) return nums[0] + nums[1] + nums[2]
+  let b = Infinity
+  let c = Infinity
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < b) {
+      c = b
+      b = nums[i]
+    } else if (nums[i] < c) c = nums[i]
+  }
+
+  return nums[0] + b + c
+}
