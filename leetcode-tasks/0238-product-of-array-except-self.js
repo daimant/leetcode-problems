a8acd1b4b6585ecc5a1cb70a8dfc959d248cd99b
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-16 15:13:28 +0500

    added valid solution for problem 238 (mid) https://leetcode.com/problems/product-of-array-except-self

--- leetcode-tasks/0238-product-of-array-except-self.js
+++ leetcode-tasks/0238-product-of-array-except-self.js
@@ -14,3 +14,21 @@ const productExceptSelf2 = (nums) => {
   const product = nums.reduce((acc, curr) => acc * curr, 0)
   return nums.map(el => product / el)
 }
+
+// Accepted
+// Runtime 74 ms Beats 94.99% of users with JavaScript
+// Memory 60.54 MB Beats 59.40% of users with JavaScript
+
+const productExceptSelf3 = (nums) => {
+  let countZero = 0
+  let product = 1
+
+  for (let i = 0; i < nums.length && countZero < 2; i++) {
+    if (nums[i] === 0) countZero++
+    else product *= nums[i]
+  }
+
+  if (countZero >= 2) return nums.map(() => 0)
+  else if (countZero === 1) return nums.map(el => (el === 0) ? product : 0)
+  else return nums.map(el => product / el)
+}
