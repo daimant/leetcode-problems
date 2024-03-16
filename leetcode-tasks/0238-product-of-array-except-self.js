a627d7c7593d6c53ef83f1dd8c0b744490de8b5d
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-16 15:12:18 +0500

    added wrong solution for problem 238 (mid) https://leetcode.com/problems/product-of-array-except-self

--- leetcode-tasks/0238-product-of-array-except-self.js
+++ leetcode-tasks/0238-product-of-array-except-self.js
@@ -6,6 +6,11 @@ var productExceptSelf = function(nums) {
     el.splice(i, 1);
     return el.reduce((prev, curr) => (curr *= prev));
   });
-};
+}
 
-// var productExceptSelf = (nums) => nums.map((el, i, arr) => {el = arr.slice(); el.splice(i,1); return el.reduce((prev, curr) => curr *= prev)});
+// wrong solution
+
+const productExceptSelf2 = (nums) => {
+  const product = nums.reduce((acc, curr) => acc * curr, 0)
+  return nums.map(el => product / el)
+}
