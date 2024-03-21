280c14ffff1c5c7d8dbaa1571bbd656d7ade9ca1
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-21 23:46:00 +0500

    added optimized solution for problem 3083 (easy) https://leetcode.com/problems/existence-of-a-substring-in-a-string-and-its-reverse

--- leetcode-tasks/3083-existence-of-a-substring-in-a-string-and-its-reverse.ts
+++ leetcode-tasks/3083-existence-of-a-substring-in-a-string-and-its-reverse.ts
@@ -9,3 +9,23 @@ const isSubstringPresent = (s: string): boolean => {
 
   return false
 }
+
+// added optimized solution
+// Accepted
+// Runtime 53 ms Beats 94.83% of users with TypeScript
+// Memory 52.69 MB Beats 34.48% of users with TypeScript
+
+const isSubstringPresent2 = (s: string): boolean => {
+  const set = new Set()
+
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] === s[i + 1] || s[i + 1] === s[i - 1]) return true
+    set.add(`${s[i + 1]}${s[i]}`)
+  }
+
+  for (let i = 0; i < s.length - 3; i++) {
+    if (set.has(`${s[i]}${s[i + 1]}`)) return true
+  }
+
+  return false
+}
