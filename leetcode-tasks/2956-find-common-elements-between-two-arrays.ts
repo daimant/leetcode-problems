13fee29356f3addb8b2b2b267dbc87a18d52cc30
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-26 18:26:38 +0500

    added second wrong solution for problem 2956 (easy) https://leetcode.com/problems/find-common-elements-between-two-arrays

--- leetcode-tasks/2956-find-common-elements-between-two-arrays.ts
+++ leetcode-tasks/2956-find-common-elements-between-two-arrays.ts
@@ -9,3 +9,20 @@ const findIntersectionValues = (nums1: number[], nums2: number[]): number[] => {
 
   return [0, 0]
 }
+
+// second wrong solution
+
+const findIntersectionValues2 = (nums1: number[], nums2: number[]): number[] => {
+  const map = new Map()
+
+  for (let i = 0; i < nums1.length; i++) {
+    map.set(nums1[i], map.has(nums1[i]) ? -1 : i)
+  }
+
+  for (let i = 0; i < nums2.length; i++) {
+    const get = map.get(nums2[i])
+    if (get && get >= 0) return [map.get(nums2[i]), i]
+  }
+
+  return [0, 0]
+}
