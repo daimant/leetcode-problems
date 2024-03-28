905375b484db2d2ef38d393bc8df99e41d83dcf4
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-28 23:02:11 +0500

    added valid solution for problem 3090 (easy) https://leetcode.com/problems/maximum-length-substring-with-two-occurrences

--- leetcode-tasks/3090-maximum-length-substring-with-two-occurrences.ts
+++ leetcode-tasks/3090-maximum-length-substring-with-two-occurrences.ts
@@ -19,3 +19,26 @@ const maximumLengthSubstring = (s: string): number => {
 
   return result
 }
+
+// Accepted
+// Runtime 73 ms Beats 71.05% of users with TypeScript
+// Memory 56.10 MB Beats 52.63% of users with TypeScript
+
+const maximumLengthSubstring2 = (s: string): number => {
+  let result = 0
+
+  for (let i = 0; i < s.length - 1; i++) {
+    const rep = new Map()
+
+    for (let j = i; j < s.length; j++) {
+      rep.set(s[j], ( rep.get(s[j]) || 0 ) + 1)
+
+      if (rep.get(s[j]) > 2) {
+        if (result < j - i) result = j - i
+        break
+      } else if (result < j - i + 1) result = j - i + 1
+    }
+  }
+
+  return result
+}
