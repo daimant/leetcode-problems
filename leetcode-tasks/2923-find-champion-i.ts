6be875efa251b6a6cd1eea5716ed84dcbf3cadba
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-30 08:48:11 +0500

    added new time for problem 2923 (easy) https://leetcode.com/problems/find-champion-i

--- leetcode-tasks/2923-find-champion-i.ts
+++ leetcode-tasks/2923-find-champion-i.ts
@@ -1,5 +1,5 @@
 // Accepted
-// Runtime 64 ms Beats 85.71% of users with TypeScript
-// Memory 54.31 MB Beats 100.00% of users with TypeScript
+// Runtime 61 ms Beats 85.71% of users with TypeScript
+// Memory 54.60 MB Beats 95.24% of users with TypeScript
 
 const findChampion = (grid: number[][]): number => grid.map(el => el.reduce((acc, el) => acc + el, 0)).reduce((acc, el, i) => el > acc[0] ? [el, i] : acc, [0, 0])[1]
