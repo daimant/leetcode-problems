83a4d1c991042c94a8217a69d6a7b38632c01414
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-31 14:37:00 +0500

    added wrong solution for problem 2917 (easy) https://leetcode.com/problems/find-the-k-or-of-an-array

---
+++ leetcode-tasks/2917-find-the-k-or-of-an-array.ts
@@ -0,0 +1,26 @@
+// wrong 950 / 951 testcases passed
+
+const findKOr = (nums: number[], k: number): number => {
+  const result = []
+  let maxBinLen = 0
+
+  const bins = nums.map(el => {
+    const currBin = []
+
+    while (el) {
+      currBin.push(el % 2)
+      el = Math.trunc(el / 2)
+    }
+
+    if (maxBinLen < currBin.length) maxBinLen = currBin.length
+
+    return currBin
+  })
+
+  for (let i = 0; i < maxBinLen; i++) {
+    let currBin = bins.map(el => el[i]).filter(el => el).length
+    result.push(currBin < k ? 0 : 1)
+  }
+
+  return Number.parseInt(result.reverse().join(''), 2)
+}
