03ffaece8af7603cc3e359a009f4e404b1e2c16f
Sergey Pomortsev <zaooooza@gmail.com>
2024-03-31 14:37:18 +0500

    added correct solution for problem 2917 (easy) https://leetcode.com/problems/find-the-k-or-of-an-array

--- leetcode-tasks/2917-find-the-k-or-of-an-array.ts
+++ leetcode-tasks/2917-find-the-k-or-of-an-array.ts
@@ -24,3 +24,32 @@ const findKOr = (nums: number[], k: number): number => {
 
   return Number.parseInt(result.reverse().join(''), 2)
 }
+
+// Accepted
+// Runtime 104 ms Beats 6.67% of users with TypeScript
+// Memory 58.72 MB Beats 6.67% of users with TypeScript
+
+const findKOr2 = (nums: number[], k: number): number => {
+  const result = []
+  let maxBinLen = 0
+
+  const bins = nums.map(el => {
+    const currBin = []
+
+    while (el) {
+      currBin.push(el % 2)
+      el = Math.trunc(el / 2)
+    }
+
+    if (maxBinLen < currBin.length) maxBinLen = currBin.length
+
+    return currBin
+  })
+
+  for (let i = 0; i < maxBinLen; i++) {
+    let currBin = bins.map(el => el[i]).filter(el => el).length
+    result.push(currBin < k ? 0 : 1)
+  }
+
+  return result.length ? Number.parseInt(result.reverse().join(''), 2) : 0
+}
