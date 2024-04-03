77765a0d345ff712c1bd18465649eb14e0932782
Sergey Pomortsev <zaooooza@gmail.com>
2024-04-03 10:52:21 +0500

    added one-liner solution for problem 3099 (easy) https://leetcode.com/problems/harshad-number

--- leetcode-tasks/3099-harshad-number.ts
+++ leetcode-tasks/3099-harshad-number.ts
@@ -6,3 +6,10 @@ const sumOfTheDigitsOfHarshadNumber = (x: number): number => {
   const harshad = String(x).split('').reduce((acc, curr) => acc + +curr, 0)
   return x % harshad ? -1 : harshad
 }
+
+// refactored for one line
+// Accepted
+// Runtime 52 ms Beats 100.00% of users with TypeScript
+// Memory 50.13 MB Beats 100.00% of users with TypeScript
+
+const sumOfTheDigitsOfHarshadNumber2 = (x: number, harshad = String(x).split('').reduce((acc, curr) => acc + +curr, 0)): number => x % harshad ? -1 : harshad
