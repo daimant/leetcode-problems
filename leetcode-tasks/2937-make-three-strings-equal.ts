c28c62b11186d3de50e6b6aeeef633926d6f2627
Sergey Pomortsev <zaooooza@gmail.com>
2024-04-07 13:11:54 +0500

    added correct solution for problem 2937 (easy) https://leetcode.com/problems/make-three-strings-equal

--- leetcode-tasks/2937-make-three-strings-equal.ts
+++ leetcode-tasks/2937-make-three-strings-equal.ts
@@ -10,3 +10,19 @@ const findMinimumOperations = (s1: string, s2: string, s3: string): number => {
 
   return stopMatch === 0 ? -1 : s1.slice(stopMatch).length + s2.slice(stopMatch).length + s3.slice(stopMatch).length
 }
+
+// Accepted
+// Runtime 93 ms Beats 85.37% of users with TypeScript
+// Memory 54.99 MB Beats 56.10% of users with TypeScript
+
+const findMinimumOperations2 = (s1: string, s2: string, s3: string): number => {
+  const maxLen = Math.max(s1.length, s2.length, s3.length)
+  let stopMatch = -1
+
+  for (let i = 0; i < maxLen && stopMatch === -1; i++) {
+    if (s1[i] !== s2[i] || s2[i] !== s3[i] || s3[i] !== s1[i]) stopMatch = i
+    else if (i === maxLen - 1) return 0
+  }
+
+  return stopMatch === 0 ? -1 : s1.slice(stopMatch).length + s2.slice(stopMatch).length + s3.slice(stopMatch).length
+}
