c7c53ee3ea2274999e7a63f277a7d9fc1e97bd4b
Sergey Pomortsev <zaooooza@gmail.com>
2024-04-10 12:18:10 +0500

    added time for problem 950 (easy) https://leetcode.com/problems/reveal-cards-in-increasing-order

--- leetcode-tasks/0950-reveal-cards-in-increasing-order.js
+++ leetcode-tasks/0950-reveal-cards-in-increasing-order.js
@@ -1,3 +1,7 @@
+// Accepted
+// Runtime 76 ms Beats 8.41% of users with JavaScript
+// Memory 36.56 MB Beats 100.00% of users with JavaScript
+
 /**
  * @param {number[]} deck
  * @return {number[]}
