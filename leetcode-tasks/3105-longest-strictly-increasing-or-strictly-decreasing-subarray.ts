dd62d1d7a5ad7aca9223ae7c4cddb7355d4a10ae
Sergey Pomortsev <zaooooza@gmail.com>
2024-04-12 13:12:41 +0500

    added optimized solution for problem 3105 (easy) https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray

--- leetcode-tasks/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.ts
+++ leetcode-tasks/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.ts
@@ -26,3 +26,37 @@ const setSub = (sub: { length: number, type: string }, newLen: number = 0, newTy
   sub.length = newLen
   sub.type = newType
 }
+
+// optimized
+// Accepted
+// Runtime 44 ms Beats 100.00% of users with TypeScript
+// Memory 51.74 MB Beats 95.08% of users with TypeScript
+
+const longestMonotonicSubarray2 = (nums: number[]): number => {
+  let maxSubLen = 1
+  let subLen = 0
+  let subType = ''
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+      if (subType === 'dec') subLen++
+      else {
+        subLen = 2
+        subType = 'dec'
+      }
+    } else if (nums[i] < nums[i + 1]) {
+      if (subType === 'inc') subLen++
+      else {
+        subLen = 2
+        subType = 'inc'
+      }
+    } else {
+      subLen = 1
+      subType = ''
+    }
+
+    if (maxSubLen < subLen) maxSubLen = subLen
+  }
+
+  return maxSubLen
+}
