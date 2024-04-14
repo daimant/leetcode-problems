8ef2d51b374ce625494f2daf0986ad38f5c2e623
Sergey Pomortsev <zaooooza@gmail.com>
2024-04-14 10:05:19 +0500

    added valid solution for problem 2903 (easy) https://leetcode.com/problems/find-indices-with-index-and-value-difference-i

--- leetcode-tasks/2903-find-indices-with-index-and-value-difference-i.ts
+++ leetcode-tasks/2903-find-indices-with-index-and-value-difference-i.ts
@@ -9,3 +9,17 @@ const findIndices = (nums: number[], indexDifference: number, valueDifference: n
 
   return [-1, -1]
 }
+
+// Accepted
+// Runtime 56 ms Beats 83.33% of users with TypeScript
+// Memory 52.17 MB Beats 16.67% of users with TypeScript
+
+const findIndices2 = (nums: number[], indexDifference: number, valueDifference: number): number[] => {
+  for (let i = 0; i < nums.length - indexDifference; i++) {
+    for (let j = i + indexDifference; j < nums.length; j++) {
+      if (Math.abs(Math.abs(nums[i]) - Math.abs(nums[j])) >= valueDifference) return [i, j]
+    }
+  }
+
+  return [-1, -1]
+}
