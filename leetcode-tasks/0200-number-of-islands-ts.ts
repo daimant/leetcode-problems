f53d1f5d593d9e07b0752a85b68c5236e5a75932
Sergey Pomortsev <zaooooza@gmail.com>
2024-04-19 14:45:31 +0500

    added ts solution for problem 200 (medium) https://leetcode.com/problems/number-of-islands

---
+++ leetcode-tasks/0200-number-of-islands-ts.ts
@@ -0,0 +1,43 @@
+// Accepted
+// Runtime 446 ms Beats 5.00% of users with TypeScript
+// Memory 74.06 MB Beats 5.09% of users with TypeScript
+
+const numIslands = (grid: string[][]): number => {
+  if (!grid.length) return 0
+  let start = new Set(),
+    checked = new Set(),
+    needCheck = [],
+    islandCount = 0,
+    xLen = grid.length,
+    yLen = grid[0].length
+
+  const isCurrIsland = (currPoint) => {
+    start.delete(currPoint)
+    checked.add(currPoint)
+    const [x, y] = currPoint.split(',').map(el => +el)
+
+    if (x > 0 && grid[x - 1][y] === '1') needCheck.push(`${x - 1},${y}`)
+    if (x < xLen - 1 && grid[x + 1][y] === '1') needCheck.push(`${x + 1},${y}`)
+    if (y > 0 && grid[x][y - 1] === '1') needCheck.push(`${x},${y - 1}`)
+    if (y < yLen - 1 && grid[x][y + 1] === '1') needCheck.push(`${x},${y + 1}`)
+  }
+
+  for (let i = 0; i < xLen; i++) {
+    for (let j = 0; j < yLen; j++) {
+      if (grid[i][j] === '1') start.add(`${i},${j}`)
+    }
+  }
+  while (start.size) {
+    needCheck.push([...start][0])
+
+    while (needCheck.length) {
+      let currPoint = needCheck.shift()
+      if (checked.has(currPoint)) continue
+      isCurrIsland(currPoint)
+    }
+
+    islandCount++
+  }
+
+  return islandCount
+}
