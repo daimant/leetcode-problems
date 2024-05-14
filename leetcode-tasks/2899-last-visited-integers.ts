7b82f401a118d99d9842d90aa9f098e10c566c42
Sergey Pomortsev <zaooooza@gmail.com>
2024-05-14 12:45:36 +0500

    added optimized solution for problem 2899 (easy) https://leetcode.com/problems/last-visited-integers

--- leetcode-tasks/2899-last-visited-integers.ts
+++ leetcode-tasks/2899-last-visited-integers.ts
@@ -19,3 +19,26 @@ const lastVisitedIntegers = (nums: number[]): number[] => {
 
   return ans
 }
+
+// optimized
+// Accepted
+// Runtime 66 ms Beats 81.25% of users with TypeScript
+// Memory 52.79 MB Beats 93.75% of users with TypeScript
+
+const lastVisitedIntegers2 = (nums: number[]): number[] => {
+  const seen = []
+  const ans = []
+  let k = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === -1) {
+      k++
+      ans.push(seen.length >= k ? seen[seen.length - k] : -1)
+    } else {
+      k = 0
+      seen.push(nums[i])
+    }
+  }
+
+  return ans
+}
