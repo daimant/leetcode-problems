b51d07b2fa405ec6e3937f6ab087fcf5195aef74
Sergey Pomortsev <zaooooza@gmail.com>
2024-05-24 10:13:16 +0500

    added solution for problem 3131 (easy) https://leetcode.com/problems/find-the-integer-added-to-array-i

---
+++ leetcode-tasks/3131-find-the-integer-added-to-array-i.ts
@@ -0,0 +1,5 @@
+// Accepted
+// Runtime 49 ms Beats 99.40% of users with TypeScript
+// Memory 51.65 MB Beats 79.64% of users with TypeScript
+
+const addedInteger = (nums1: number[], nums2: number[]): number => Math.min(...nums2) - Math.min(...nums1)
