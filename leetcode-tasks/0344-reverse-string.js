5bf3d3a5e19e7ec57bdc8bc71ad886666c2ebdb7
Sergey Pomortsev <zaooooza@gmail.com>
2024-06-03 10:45:11 +0500

    added time for problem 0344 (easy) https://leetcode.com/problems/reverse-string

--- leetcode-tasks/0344-reverse-string.js
+++ leetcode-tasks/0344-reverse-string.js
@@ -1,3 +1,7 @@
+// Accepted
+// Runtime 70 ms Beats 88.08% of users with JavaScript
+// Memory 56.88 MB Beats 65.90% of users with JavaScript
+
 /**
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
