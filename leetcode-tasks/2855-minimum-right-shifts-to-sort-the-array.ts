535e045b398e6683fea3576692043e2ea7793b06
Sergey Pomortsev <zaooooza@gmail.com>
2024-06-07 11:50:42 +0500

    added optimized solution for problem 2855 (easy) https://leetcode.com/problems/minimum-right-shifts-to-sort-the-array

--- leetcode-tasks/2855-minimum-right-shifts-to-sort-the-array.ts
+++ leetcode-tasks/2855-minimum-right-shifts-to-sort-the-array.ts
@@ -21,3 +21,29 @@ const minimumRightShifts = (nums: number[]): number => {
 
   return nums.length - index - 1
 }
+
+// optimized
+// Accepted
+// Runtime 50 ms Beats 100.00% of users with TypeScript
+// Memory 52.46 MB Beats 18.18% of users with TypeScript
+
+const minimumRightShifts2 = (nums: number[]): number => {
+  let goalArr = []
+  let goalIndex = 0
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+      if (goalArr.length) return -1
+      goalIndex = i + 1
+      goalArr = [...nums.slice(goalIndex), ...nums.slice(0, goalIndex)]
+    }
+  }
+
+  if (!goalArr.length) return 0
+
+  for (let i = 0; i < goalArr.length - 1; i++) {
+    if (goalArr[i] > goalArr[i + 1]) return -1
+  }
+
+  return goalArr.length - goalIndex
+}
