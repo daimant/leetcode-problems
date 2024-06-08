a975b2222bfae8f9c73056d265e8e05cc0c8202e
Sergey Pomortsev <zaooooza@gmail.com>
2024-06-08 10:38:21 +0500

    added optimized solution for problem 648 (medium) https://leetcode.com/problems/replace-words

--- leetcode-tasks/648-replace-words.ts
+++ leetcode-tasks/648-replace-words.ts
@@ -17,3 +17,31 @@ const replaceWords = (dictionary: string[], sentence: string): string => {
 
   return sentenceArr.join(' ')
 }
+
+// Accepted
+// Solution Runtime 86 ms Beats 100.00% of users with TypeScript
+// Memory 62.72 MB Beats 60.00% of users with TypeScript
+
+const replaceWords2 = (dictionary: string[], sentence: string): string => {
+  const dictionaryMap = new Map()
+  const sentenceArr = sentence.split(' ')
+
+  for (let i = 0; i < dictionary.length; i++) {
+    dictionaryMap.set(
+      dictionary[i].length,
+      (dictionaryMap.has(dictionary[i].length) ? dictionaryMap.get(dictionary[i].length) : new Set()).add(dictionary[i])
+    )
+  }
+
+  for (let i = 0; i < sentenceArr.length; i++) {
+    for (let j = 0; j < sentenceArr[i].length; j++) {
+      if (!dictionaryMap.has(j + 1)) continue
+      const currSubSentence = sentenceArr[i].slice(0, j + 1)
+      if (dictionaryMap.get(j + 1).has(currSubSentence)) {
+        sentenceArr[i] = currSubSentence
+      }
+    }
+  }
+
+  return sentenceArr.join(' ')
+}
