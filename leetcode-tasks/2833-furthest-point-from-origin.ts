9b940ecba4a0a78c2d85ca101af0ceca622add34
Sergey Pomortsev <zaooooza@gmail.com>
2024-06-22 08:20:39 +0500

    added correct solution for problem 2833 (easy) https://leetcode.com/problems/furthest-point-from-origin

--- leetcode-tasks/2833-furthest-point-from-origin.ts
+++ leetcode-tasks/2833-furthest-point-from-origin.ts
@@ -20,3 +20,30 @@ const furthestDistanceFromOrigin = (moves: string): number => {
 
   return Math.abs(result)
 }
+
+// Accepted
+// Runtime 62 ms Beats 100.00%
+// Memory 54.67 MB Beats 18.92%
+
+const furthestDistanceFromOrigin2 = (moves: string): number => {
+  return Math.max(measureDistance(moves, 'R'), measureDistance(moves, 'L'))
+}
+
+const measureDistance = (moves: string, direction: string) => {
+  let result = 0
+
+  for (let i = 0; i < moves.length; i++) {
+    if (moves[i] === 'R') {
+      result++
+      if (direction !== 'R') direction !== 'R'
+    } else if (moves[i] === 'L') {
+      result--
+      if (direction !== 'L') direction !== 'L'
+    } else {
+      if (direction === 'R') result++
+      else result--
+    }
+  }
+
+  return Math.abs(result)
+}
