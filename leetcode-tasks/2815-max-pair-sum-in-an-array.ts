e8b1016d86e11f0c288b616401a35dff17250622
Sergey Pomortsev <zaooooza@gmail.com>
2024-06-25 09:43:08 +0500

    added wrong solution for problem 2815 (easy) https://leetcode.com/problems/max-pair-sum-in-an-array

---
+++ leetcode-tasks/2815-max-pair-sum-in-an-array.ts
@@ -0,0 +1,27 @@
+// Wrong Answer
+// 2968 / 3008 testcases passed
+
+const maxSum = (nums: number[]): number => {
+  const map = new Map()
+
+  nums.forEach(el => {
+    const currMax = Math.max(...String(el).split('').map(el => +el))
+    let acc = map.get(currMax)
+
+    if (!acc) acc = [el]
+    else if (acc.length === 1) {
+      if (acc[0] > el) acc.push(el)
+      else acc.unshift(el)
+    } else {
+      if (el > acc[0]) {
+        acc[1] = acc[0]
+        acc[0] = el
+      } else if (el < acc[0] && el > acc[1]) acc[1] = el
+    }
+
+    map.set(currMax, acc)
+  })
+
+  const arr = Array.from(map.values())
+  return Math.max(...arr.map(inArr => inArr.length === 1 ? -1 : inArr[0] + inArr[1]))
+}
