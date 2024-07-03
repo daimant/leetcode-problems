94d88b4bdec08c97004e6160bf72034f83ca153a
Sergey Pomortsev <zaooooza@gmail.com>
2024-07-03 12:49:53 +0500

    added valid solution for problem 2760 (easy) https://leetcode.com/problems/longest-even-odd-subarray-with-threshold

--- leetcode-tasks/2760-longest-even-odd-subarray-with-threshold.ts
+++ leetcode-tasks/2760-longest-even-odd-subarray-with-threshold.ts
@@ -65,3 +65,27 @@ const longestAlternatingSubarray3 = (nums: number[], threshold: number): number 
 
   return max
 }
+
+// Accepted
+// Runtime 113 ms Beats 91.67%
+// Memory 57.30 MB Beats 100.00%
+
+const longestAlternatingSubarray4 = (nums: number[], threshold: number): number => {
+  let max = 0
+  let currSeq = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > threshold) currSeq = 0
+    else if (currSeq === 0 && nums[i] % 2 !== 0) currSeq = 0
+    else if (currSeq !== 0) {
+      if (nums[i] % 2 === 0 && nums[i - 1] % 2 !== 0) currSeq++
+      else if (nums[i] % 2 !== 0 && nums[i - 1] % 2 === 0) currSeq++
+      else currSeq = 0
+    }
+
+    if (nums[i] <= threshold && currSeq === 0 && nums[i] % 2 === 0) currSeq++
+    if (currSeq > max) max = currSeq
+  }
+
+  return max
+}
