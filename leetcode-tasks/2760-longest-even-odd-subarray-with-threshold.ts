e03600a3d35eb4d3fe0b4ecaf5845a89def4591e
Sergey Pomortsev <zaooooza@gmail.com>
2024-07-03 12:49:29 +0500

    added third wrong solution for problem 2760 (easy) https://leetcode.com/problems/longest-even-odd-subarray-with-threshold

--- leetcode-tasks/2760-longest-even-odd-subarray-with-threshold.ts
+++ leetcode-tasks/2760-longest-even-odd-subarray-with-threshold.ts
@@ -42,3 +42,26 @@ const longestAlternatingSubarray2 = (nums: number[], threshold: number): number 
 
   return max
 }
+
+// Wrong Answer
+// 6349 / 6873 testcases passed
+
+const longestAlternatingSubarray3 = (nums: number[], threshold: number): number => {
+  let max = 0
+  let currSeq = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > threshold) currSeq = 0
+    else if (currSeq === 0 && nums[i] % 2 !== 0) currSeq = 0
+    else if (currSeq !== 0) {
+      if (nums[i] % 2 === 0 && nums[i - 1] % 2 !== 0) currSeq++
+      else if (nums[i] % 2 !== 0 && nums[i - 1] % 2 === 0) currSeq++
+      else currSeq = 0
+    }
+
+    if (currSeq === 0 && nums[i] % 2 === 0) currSeq++
+    if (currSeq > max) max = currSeq
+  }
+
+  return max
+}
