2a73228730837a84529c5604360685f3d7c44517
Sergey Pomortsev <zaooooza@gmail.com>
2024-07-04 12:29:11 +0500

    added valid solution for problem 2748 (easy) https://leetcode.com/problems/number-of-beautiful-pairs

--- leetcode-tasks/2748-number-of-beautiful-pairs.ts
+++ leetcode-tasks/2748-number-of-beautiful-pairs.ts
@@ -32,3 +32,21 @@ const countBeautifulPairs2 = (nums: number[]): number => {
 
   return count
 }
+
+// Accepted
+// Runtime 133 ms Beats 90.00%
+// Memory 58.81 MB Beats 10.00%
+
+const countBeautifulPairs3 = (nums: number[]): number => {
+  let count = 0
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      const [xLong, yLong] = [String(nums[i]), String(nums[j])]
+      const [x, y] = [+xLong[0], +yLong[yLong.length - 1]]
+      if (!(x % 2 === 0 && y % 2 === 0) && !(x % 3 === 0 && y % 3 === 0) && !(x % 5 === 0 && y % 5 === 0) && !(x % 7 === 0 && y % 7 === 0)) count++
+    }
+  }
+
+  return count
+}
