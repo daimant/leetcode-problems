b6777dac48ab9b30b01fa363f816dfc2f208991f
Sergey Pomortsev <zaooooza@gmail.com>
2024-07-24 22:30:23 +0500

    added wrong answer for problem 2729 (easy) https://leetcode.com/problems/check-if-the-number-is-fascinating

--- leetcode-tasks/2729-check-if-the-number-is-fascinating.ts
+++ leetcode-tasks/2729-check-if-the-number-is-fascinating.ts
@@ -1,8 +1,16 @@
+// Wrong Answer
+// 894 / 900 testcases passed
+
+const isFascinating = (n: number): boolean =>
+  new Set([...`${n}${n * 2}${n * 3}`]).size === 9
+  && `${n}${n * 2}${n * 3}`.indexOf('0') === -1
+
+
 // Accepted
 // Runtime 59 ms Beats 85.71%
 // Memory 51.69 MB Beats 71.43%
 
-const isFascinating = (n: number): boolean =>
+const isFascinating2 = (n: number): boolean =>
   new Set([...`${n}${n * 2}${n * 3}`]).size === 9
   && `${n}${n * 2}${n * 3}`.indexOf('0') === -1
   && `${n}${n * 2}${n * 3}`.length === 9
