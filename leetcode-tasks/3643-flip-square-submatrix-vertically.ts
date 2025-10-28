9e2a7d84d7f00c57e452611bf45e21a2e075d520
Sergey Pomortsev <zaooooza@gmail.com>
2025-10-28 15:05:40 +0300

    added other solution for problem 3643 (easy) https://leetcode.com/problems/flip-square-submatrix-vertically

--- leetcode-tasks/3643-flip-square-submatrix-vertically.ts
+++ leetcode-tasks/3643-flip-square-submatrix-vertically.ts
@@ -12,3 +12,18 @@ const reverseSubmatrix = (grid: number[][], x: number, y: number, k: number): nu
 
   return grid
 }
+
+
+// Diff way to equate - better by memory
+// Accepted 674 / 674 testcases passed Sergey Pomortsev submitted at Oct 26, 2025 17:05
+// Solution Runtime 0 ms Beats 100.00% Analyze Complexity Memory 64.68 MB Beats 91.67%
+
+const reverseSubmatrix2 = (grid: number[][], x: number, y: number, k: number): number[][] => {
+  for (let row = x; row < x + Math.trunc(k / 2); row++) {
+    for (let col = y; col < y + k; col++) {
+      [grid[row][col], grid[x + k - 1 - (row - x)][col]] = [grid[x + k - 1 - (row - x)][col], grid[row][col]]
+    }
+  }
+
+  return grid
+}
