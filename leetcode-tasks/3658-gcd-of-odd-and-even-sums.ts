faca441991914cc31da67c3233dfdb40bf04e717
Sergey Pomortsev <zaooooza@gmail.com>
2025-10-29 13:52:56 +0300

    added solution for problem 3658 (easy) https://leetcode.com/problems/gcd-of-odd-and-even-sums

---
+++ leetcode-tasks/3658-gcd-of-odd-and-even-sums.ts
@@ -0,0 +1,12 @@
+// Accepted 1000 / 1000 testcases passed Sergey Pomortsev submitted at Oct 26, 2025 12:01
+// Solution Runtime 9 ms Beats 18.37% Analyze Complexity Memory 60.30 MB Beats 5.10%
+
+const gcdOfOddEvenSums = (n: number): number => {
+  const [oddSum, evenSum] = new Array(n * 2).fill(1).reduce((acc, _, i) => (i + 1) % 2 ? [acc[0] + i + 1, acc[1]] : [acc[0], acc[1] + i + 1], [0, 0])
+
+  for (let i = Math.min(oddSum, evenSum); i > 0; i--) {
+    if (oddSum % i === 0 && evenSum % i === 0) return i
+  }
+
+  return 1
+}
