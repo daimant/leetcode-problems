0cb537a91ab02370c913252d8f9270e7ccc9d6de
Sergey Pomortsev <zaooooza@gmail.com>
2025-11-07 18:17:47 +0300

    added one-line solution for problem 3674 (easy) https://leetcode.com/problems/minimum-operations-to-equalize-array

--- tasks/3674-minimum-operations-to-equalize-array.ts
+++ tasks/3674-minimum-operations-to-equalize-array.ts
@@ -7,3 +7,9 @@ const minOperations4 = (nums: number[]): number => {
   }
   return 0
 }
+
+// Added one-line solution
+// Accepted 989 / 989 testcases passed Sergey Pomortsev submitted at Oct 25, 2025 14:41
+// Solution Runtime 0 ms Beats 100.00% Analyze Complexity Memory 58.05 MB Beats 38.20%
+
+const minOperations5 = (nums: number[]): number => new Set(nums).size === 1 ? 0 : 1
