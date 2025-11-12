274a46beccc47400801a3d70cc69a08f6e983f80
Sergey Pomortsev <zaooooza@gmail.com>
2025-11-12 17:05:41 +0300

    added faster solution for problem 3678 (easy) https://leetcode.com/problems/smallest-absent-positive-greater-than-average

--- tasks/3678-smallest-absent-positive-greater-than-average.ts
+++ tasks/3678-smallest-absent-positive-greater-than-average.ts
@@ -9,3 +9,23 @@ const smallestAbsent = (nums: number[]): number => {
     if (!uniq.has(i)) return i
   }
 };
+
+// Added faster solution
+// Accepted 3500 / 3500 testcases passed Sergey Pomortsev submitted at Oct 24, 2025 14:56
+// Solution Runtime 5 ms Beats 77.42% Analyze Complexity Memory 64.49 MB Beats 34.41%
+
+const smallestAbsent2 = (nums: number[]): number => {
+  const uniq = new Set()
+  let div = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    uniq.add(nums[i])
+    div += nums[i]
+  }
+
+  div /= nums.length
+
+  for (let i = div > 0 ? Math.trunc(div) + 1 : 1; true; i++) {
+    if (!uniq.has(i)) return i
+  }
+};
