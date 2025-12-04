a84c4a5f5ce857c9e6478e0b01756c18d37a7b32
Sergey Pomortsev <zaooooza@gmail.com>
2025-12-04 22:01:11 +0300

    added solution for problem 3760 (medium) https://leetcode.com/problems/maximum-substrings-with-distinct-start

---
+++ tasks/3760-maximum-substrings-with-distinct-start.ts
@@ -0,0 +1,4 @@
+// Accepted 1163 / 1163 testcases passed Sergey Pomortsev submitted at Dec 02, 2025 14:06
+// Solution Runtime 39 ms Beats 31.58% Analyze Complexity Memory 66.33 MB Beats 26.32%
+
+const maxDistinct = (s: string): number => new Set([...s]).size
