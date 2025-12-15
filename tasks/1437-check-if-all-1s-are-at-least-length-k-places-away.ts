79262d422bc54ac00cf1be8434432972be9d097b
Sergey Pomortsev <zaooooza@gmail.com>
2025-12-15 11:09:46 +0300

    added solution for problem 1437 (easy) https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away

--- tasks/1437-check-if-all-1s-are-at-least-length-k-places-away.ts
+++ tasks/1437-check-if-all-1s-are-at-least-length-k-places-away.ts
@@ -1,11 +1,12 @@
-"use strict";
+// Accepted 62 / 62 testcases passed Sergey Pomortsev submitted at May 06, 2020 23:03  Editorial
+// Solution Runtime 84 ms Beats 6.15% Analyze Complexity Memory 41.66 MB Beats 100.00%
 
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {boolean}
  */
-var kLengthApart = function(nums, k) {
+var kLengthApart = function (nums, k) {
   let count = -1;
 
   for (let i = 0; i < nums.length; i++) {
