475efd543142d4ec144c2f8f571f5739a84ad6ce
Sergey Pomortsev <zaooooza@gmail.com>
2026-01-07 09:05:17 +0500

    added solution for problem 3746 (medium) https://leetcode.com/problems/minimum-string-length-after-balanced-removals

---
+++ tasks/3746-minimum-string-length-after-balanced-removals.ts
@@ -0,0 +1,12 @@
+// Accepted 680 / 680 testcases passed Sergey Pomortsev submitted at Dec 04, 2025 21:49
+// Solution Runtime 14 ms Beats 11.39% Analyze Complexity Memory 59.22 MB Beats 59.49%
+
+function minLengthAfterRemovals(s: string): number {
+  const map = new Map()
+
+  for (let i = 0; i < s.length; i++) {
+    map.set(s[i], map.has(s[i]) ? map.get(s[i]) + 1 : 1)
+  }
+
+  return Math.abs((map.get('a') || 0) - (map.get('b') || 0))
+};
