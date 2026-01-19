0c3fc2ef545628e8c2295f5cab55c752fe7bfb8e
Sergey Pomortsev <zaooooza@gmail.com>
2026-01-19 08:31:12 +0500

    added other solution for problem 3683 (easy) https://leetcode.com/problems/earliest-time-to-finish-one-task

--- tasks/3683-earliest-time-to-finish-one-task.ts
+++ tasks/3683-earliest-time-to-finish-one-task.ts
@@ -2,3 +2,17 @@
 // Solution Runtime 0 ms Beats 100.00% Analyze Complexity Memory 58.93 MB Beats 68.15%
 
 const earliestTime = (tasks: number[][]): number => Math.min(...tasks.map(el => el[0] + el[1]))
+
+// Other solution
+// Accepted 913 / 913 testcases passed Sergey Pomortsev submitted at Oct 24, 2025 14:39
+// Solution Runtime 0 ms Beats 100.00% Analyze Complexity Memory 58.35 MB Beats 86.62%
+
+const earliestTime2 = (tasks: number[][]): number => {
+  let result = Infinity
+
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i][0] + tasks[i][1] < result) result = tasks[i][0] + tasks[i][1]
+  }
+
+  return result
+}
