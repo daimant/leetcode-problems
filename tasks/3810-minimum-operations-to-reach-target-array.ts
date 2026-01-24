62aa90f5981148047af541863b6c0c9266f7b7d5
Sergey Pomortsev <zaooooza@gmail.com>
2026-01-24 18:12:35 +0500

    added information for problem 3810 (medium) https://leetcode.com/problems/minimum-operations-to-reach-target-array

--- tasks/3810-minimum-operations-to-reach-target-array.ts
+++ tasks/3810-minimum-operations-to-reach-target-array.ts
@@ -1,3 +1,6 @@
+// Accepted 1014 / 1014 testcases passed Sergey Pomortsev submitted at Jan 23, 2026 16:50
+// Solution Runtime 27 ms Beats 33.33% Analyze Complexity Memory 83.69 MB Beats 89.74%
+
 function minOperations6(nums: number[], target: number[]): number {
   const unique = new Set()
   let count = 0
