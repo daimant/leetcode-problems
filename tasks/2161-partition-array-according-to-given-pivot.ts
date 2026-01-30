d7859e09364061bc67fc7485b9741227a39928b6
Sergey Pomortsev <zaooooza@gmail.com>
2026-01-30 12:51:41 +0500

    added optimized solution for problem 2161 (medium) https://leetcode.com/problems/partition-array-according-to-given-pivot

--- tasks/2161-partition-array-according-to-given-pivot.ts
+++ tasks/2161-partition-array-according-to-given-pivot.ts
@@ -4,3 +4,21 @@
 function pivotArray(nums: number[], pivot: number): number[] {
   return [...nums.filter(el => el < pivot), ...nums.filter(el => el === pivot), ...nums.filter(el => el > pivot)]
 };
+
+// optimized solution
+// Accepted 44 / 44 testcases passed Sergey Pomortsev submitted at Jan 30, 2026 12:49
+// Solution Runtime 16 ms Beats 84.81% Analyze Complexity Memory 81.48 MB Beats 100.00%
+
+function pivotArray(nums: number[], pivot: number): number[] {
+  const lessByPivot: number[] = []
+  const greatByPivot: number[] = []
+  let pivotCount = 0
+
+  nums.forEach(el => {
+    if (el < pivot) lessByPivot.push(el)
+    else if (el > pivot) greatByPivot.push(el)
+    else pivotCount++
+  })
+
+  return lessByPivot.concat(new Array(pivotCount).fill(pivot), greatByPivot)
+};
