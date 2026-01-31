ee8a16c894911a62c9f262fa03d39928f7e0f49d
Sergey Pomortsev <zaooooza@gmail.com>
2026-02-01 00:19:23 +0500

    added solution for problem 2396 (medium) https://leetcode.com/problems/strictly-palindromic-number

---
+++ tasks/2396-strictly-palindromic-number.ts
@@ -0,0 +1,4 @@
+// Accepted 67 / 67 testcases passed Sergey Pomortsev submitted at Jan 29, 2026 17:00
+// Solution Runtime 0 ms Beats 100.00% Analyze Complexity Memory 53.68 MB Beats 95.08%
+
+const isStrictlyPalindromic = (n: number): boolean => false
