52ff8680421ff3ae21cdc5e84e8e7b1478652e88
Sergey Pomortsev <zaooooza@gmail.com>
2026-02-05 12:32:51 +0500

    added optimized solution for problem 1637 (easy) https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points

--- tasks/1637-widest-vertical-area-between-two-points-containing-no-points.ts
+++ tasks/1637-widest-vertical-area-between-two-points-containing-no-points.ts
@@ -12,3 +12,19 @@ function maxWidthOfVerticalArea(points: number[][]): number {
 
   return maxLen
 };
+
+// optimize
+// Accepted 56 / 56 testcases passed Sergey Pomortsev submitted at Feb 03, 2026 18:58
+// Solution Runtime 39 ms Beats 100.00% Analyze Complexity Memory 79.95 MB Beats 19.23%
+
+function maxWidthOfVerticalArea(points: number[][]): number {
+  let maxLen = 0
+  const pointsFlat = points.map(el => el[0]).sort((a, b) => a - b)
+
+  for (let i = 0; i < pointsFlat.length - 1; i++) {
+    const currDiff = pointsFlat[i + 1] - pointsFlat[i]
+    if (currDiff > maxLen) maxLen = currDiff
+  }
+
+  return maxLen
+};
