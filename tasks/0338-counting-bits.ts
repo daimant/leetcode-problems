0d27a2543d100d0fa5db9d4abc941b727995331b
Sergey Pomortsev <zaooooza@gmail.com>
2026-02-19 12:54:23 +0500

    added optimized solution for problem 0338 (easy) https://leetcode.com/problems/counting-bits

--- tasks/0338-counting-bits.ts
+++ tasks/0338-counting-bits.ts
@@ -10,3 +10,16 @@ function countBits(n: number): number[] {
 
   return resultArr
 };
+
+// optimized
+// Accepted 15 / 15 testcases passed Sergey Pomortsev submitted at Feb 17, 2026 17 : 40
+// Solution Runtime 21 ms Beats 11.98 % Analyze Complexity Memory 69.00 MB Beats 5.21 %
+function countBits(n: number): number[] {
+  const resultArr = []
+
+  for (let i = 0; i <= n; i++) {
+    resultArr.push(i.toString(2).split('').filter(el => el === '1').length)
+  }
+
+  return resultArr
+};
