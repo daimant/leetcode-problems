49e95843dec7927d84c0c94f5681b4114b86a462
Sergey Pomortsev <zaooooza@gmail.com>
2026-02-25 19:07:37 +0500

    added optimized solution for problem 1877 (medium) https://leetcode.com/problems/minimize-maximum-pair-sum-in-array

--- tasks/1877-minimize-maximum-pair-sum-in-array.ts
+++ tasks/1877-minimize-maximum-pair-sum-in-array.ts
@@ -11,3 +11,19 @@ const minPairSum = (nums: number[]): number => {
 
   return Math.max(...arr)
 }
+
+// optimized
+// Accepted 37 / 37 testcases passed Sergey Pomortsev submitted at Feb 22, 2026 22:27
+// Solution Runtime 253 ms Beats 86.61% Analyze Complexity Memory 70.80 MB Beats 55.32%
+
+const minPairSum = (nums: number[]): number => {
+  let result = -Infinity
+  nums.sort((a, b) => a - b)
+
+  for (let i = 0; i < nums.length / 2; i++) {
+    const curr = nums[i] + nums[nums.length - 1 - i]
+    if (curr > result) result = curr
+  }
+
+  return result
+}
