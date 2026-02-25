7c77b0d4e29be368d024e5613e7486d9b0bba22e
Sergey Pomortsev <zaooooza@gmail.com>
2026-02-25 19:06:32 +0500

    added optimized solution for problem 1763 (easy) https://leetcode.com/problems/longest-nice-substring

--- tasks/1763-longest-nice-substring.ts
+++ tasks/1763-longest-nice-substring.ts
@@ -28,3 +28,35 @@ function checkIsNice(str: string) {
 
   return Array.from(map.values()).every(el => el.lower && el.upper)
 }
+
+// optimized
+// Accepted 73 / 73 testcases passed Sergey Pomortsev submitted at Feb 24, 2026 21:10
+// Solution Runtime 17 ms Beats 40.00% Analyze Complexity Memory 62.55 MB Beats 12.50%
+
+function longestNiceSubstring(s: string): string {
+  let longestNice = ''
+
+  for (let i = 0; i < s.length - 1 - longestNice.length; i++) {
+    const set = new Set(s[i])
+
+    for (let j = i + 1; j < s.length; j++) {
+      set.add(s[j])
+      const isNice = checkIsNice(set)
+
+      if (isNice && j - i + 1 > longestNice.length) longestNice = s.slice(i, j + 1)
+    }
+  }
+
+  return longestNice
+};
+
+function checkIsNice(set: Set<string>) {
+  for (let el of set) {
+    const lower = el.toLowerCase()
+    const isLower = lower === el
+
+    if ((isLower && !set.has(el.toUpperCase())) || (!isLower && !set.has(lower))) return false
+  }
+
+  return true
+}
