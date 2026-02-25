c41958fabe0e3453af1e5bc34d53c0a1f3da4596
Sergey Pomortsev <zaooooza@gmail.com>
2026-02-25 19:03:39 +0500

    added unoptimized solution for problem 2200 (easy) https://leetcode.com/problems/find-all-k-distant-indices-in-an-array

--- tasks/2200-find-all-k-distant-indices-in-an-array.ts
+++ tasks/2200-find-all-k-distant-indices-in-an-array.ts
@@ -20,3 +20,23 @@ function findKDistantIndices(nums: number[], key: number, k: number): number[] {
 
   return Array.from(result)
 };
+
+// added unoptimized solution
+// Accepted 87 / 87 testcases passed Sergey Pomortsev submitted at Feb 25, 2026 18:48
+// Solution Runtime 62 ms Beats 10.00% Analyze Complexity Memory 59.27 MB Beats 55.00%
+
+function findKDistantIndices(nums: number[], key: number, k: number): number[] {
+  const result = new Set<number>()
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === key) {
+      result.add(i)
+      for (let j = 1; j <= k; j++) {
+        if (i + j < nums.length) result.add(i + j)
+        if (i - j >= 0) result.add(i - j)
+      }
+    }
+  }
+
+  return Array.from(result).sort((a, b) => a - b)
+};
