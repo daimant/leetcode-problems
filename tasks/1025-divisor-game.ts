2661db83b92f5106a44e4aa303c7da55575cff6a
Sergey Pomortsev <zaooooza@gmail.com>
2026-03-08 11:58:52 +0500

    added solution for problem 1025 (easy) https://leetcode.com/problems/divisor-game

---
+++ tasks/1025-divisor-game.ts
@@ -0,0 +1,4 @@
+// Accepted 40 / 40 testcases passed Sergey Pomortsev submitted at Mar 05, 2026 12:20
+// Solution Runtime 0 ms Beats 100.00% Analyze Complexity Memory 55.42 MB Beats 42.55%
+
+const divisorGame = (n: number): boolean => n % 2 === 0
