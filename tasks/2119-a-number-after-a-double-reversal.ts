e748210a6272b48aeef96e26b0d4aebdc9102d03
Sergey Pomortsev <zaooooza@gmail.com>
2026-03-23 19:27:49 +0500

    added solution for problem 2119 (easy) https://leetcode.com/problems/a-number-after-a-double-reversal

---
+++ tasks/2119-a-number-after-a-double-reversal.ts
@@ -0,0 +1,4 @@
+// Accepted 129 / 129 testcases passed Sergey Pomortsev submitted at Feb 23, 2026 11:25
+// Solution Runtime 0 ms Beats 100.00% Analyze Complexity Memory 55.57 MB Beats 24.14%
+
+const isSameAfterReversals = (num: number): boolean => !num || num % 10 !== 0
