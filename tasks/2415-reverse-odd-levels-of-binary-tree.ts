132d091d85110bd585c807608df612a9b27653e2
Sergey Pomortsev <zaooooza@gmail.com>
2026-04-01 14:28:11 +0500

    added optimized solution for problem 2415 (medium) https://leetcode.com/problems/reverse-odd-levels-of-binary-tree

--- tasks/2415-reverse-odd-levels-of-binary-tree.ts
+++ tasks/2415-reverse-odd-levels-of-binary-tree.ts
@@ -51,3 +51,51 @@ const replaceTreeVals = (link: TreeNode | null, level: number, position: number,
   replaceTreeVals(link.left, level + 1, (position * 2) - 1, map)
   replaceTreeVals(link.right, level + 1, position * 2, map)
 }
+
+// optimize
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function reverseOddLevels(root: TreeNode | null): TreeNode | null {
+  const map = new Map()
+
+  checkTreeLevels(root, 0, 1, map)
+  replaceTreeVals(root, 0, 1, map)
+
+  return root
+};
+
+const checkTreeLevels = (link: TreeNode | null, level: number, position: number, map: Map<number, number[]>) => {
+  if (!link) return
+
+  if (level % 2) {
+    let arr = []
+    if (map.has(level)) arr = map.get(level)
+    arr[position - 1] = link.val
+    map.set(level, arr)
+  }
+
+  checkTreeLevels(link.left, level + 1, (position * 2) - 1, map)
+  checkTreeLevels(link.right, level + 1, position * 2, map)
+}
+
+const replaceTreeVals = (link: TreeNode | null, level: number, position: number, map: Map<number, number[]>) => {
+  if (!link) return
+
+  if (level % 2) link.val = map.get(level)[map.get(level).length - 1 - (position - 1)]
+
+  replaceTreeVals(link.left, level + 1, (position * 2) - 1, map)
+  replaceTreeVals(link.right, level + 1, position * 2, map)
+}
