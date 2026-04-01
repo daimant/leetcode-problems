284a9b2c05b4f2e961d3f33f2d6c408fe419dec6
Sergey Pomortsev <zaooooza@gmail.com>
2026-04-01 14:29:21 +0500

    added optimized solution for problem 2482 (medium) https://leetcode.com/problems/difference-between-ones-and-zeros-in-row-and-column

--- tasks/2482-difference-between-ones-and-zeros-in-row-and-column.ts
+++ tasks/2482-difference-between-ones-and-zeros-in-row-and-column.ts
@@ -34,3 +34,38 @@ function onesMinusZeros(grid: number[][]): number[][] {
 
   return grid;
 };
+
+// optimized
+// Accepted 85 / 85 testcases passed Sergey Pomortsev submitted at Feb 25, 2026 21:46
+// Solution Runtime 53 ms Beats 85.71% Analyze Complexity Memory 94.60 MB Beats 42.86%
+
+function onesMinusZeros(grid: number[][]): number[][] {
+  const colsMap = new Map<number, { ones: number, zeroes: number }>();
+  const rowsMap = new Map<number, { ones: number, zeroes: number }>();
+
+  for (let i = 0; i < grid.length; i++) {
+    let count = 0;
+    for (let j = 0; j < grid[0].length; j++) {
+      count += grid[i][j];
+    }
+    rowsMap.set(i, { ones: count, zeroes: grid[0].length - count });
+  }
+
+  for (let j = 0; j < grid[0].length; j++) {
+    let count = 0;
+    for (let i = 0; i < grid.length; i++) {
+      count += grid[i][j];
+    }
+    colsMap.set(j, { ones: count, zeroes: grid.length - count });
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      const getCol = colsMap.get(j);
+      const getRow = rowsMap.get(i);
+      grid[i][j] = getCol.ones + getRow.ones - getCol.zeroes - getRow.zeroes;
+    }
+  }
+
+  return grid;
+};
