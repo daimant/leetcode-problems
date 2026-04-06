4c4c13f864a7615e997c0a984817da6dacf38f23
Sergey Pomortsev <zaooooza@gmail.com>
2026-04-06 20:19:59 +0500

    added correct solution for problem 2506 (easy) https://leetcode.com/problems/count-pairs-of-similar-strings

--- tasks/2506-count-pairs-of-similar-strings.ts
+++ tasks/2506-count-pairs-of-similar-strings.ts
@@ -14,3 +14,31 @@ const similarPairs = (words: string[]): number => {
 
   return result === 1 ? 0 : result
 };
+
+// Accepted 225 / 225 testcases passed Sergey Pomortsev submitted at Mar 03, 2026 23:50
+// Solution Runtime 6 ms Beats 93.33% Analyze Complexity Memory 60.76 MB Beats 86.67%
+
+function similarPairs(words: string[]): number {
+  const map = new Map<string, number>();
+
+  for (let i = 0; i < words.length; i++) {
+    const currWord = [];
+
+    for (let j = 0; j < words[i].length; j++) {
+      currWord[words[i][j].charCodeAt(0) - 97] = words[i][j];
+    }
+
+    const currStr = currWord.join('')
+    map.set(currStr, map.has(currStr) ? map.get(currStr) + 1 : 1)
+  }
+
+  return Array
+    .from(map.values())
+    .reduce((acc, curr) => {
+      let count = 0;
+      for (let i = 1; i <= curr; i++) {
+        count += i - 1;
+      }
+      return acc + count;
+    }, 0);
+};
