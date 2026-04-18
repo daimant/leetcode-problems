6d0ba171bc6b31077302f909d93e3758698256bf
Sergey Pomortsev <zaooooza@gmail.com>
2026-04-18 08:32:43 +0500

    added solution for problem 2651 (easy) https://leetcode.com/problems/calculate-delayed-arrival-time

---
+++ tasks/2651-calculate-delayed-arrival-time.ts
@@ -0,0 +1,4 @@
+// Accepted 556 / 556 testcases passed Sergey Pomortsev submitted at Mar 02, 2026 12:40
+// Solution Runtime 0 ms Beats 100.00% Analyze Complexity Memory 56.92 MB Beats 6.67%
+
+const findDelayedArrivalTime = (arrivalTime: number, delayedTime: number): number => (arrivalTime + delayedTime) % 24
