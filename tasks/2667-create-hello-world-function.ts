5399670b6bae933bdcd934401769fcc7812cfdca
Sergey Pomortsev <zaooooza@gmail.com>
2026-04-22 15:26:43 +0500

    added solution for problem 2666 (easy) https://leetcode.com/problems/create-hello-world-function

---
+++ tasks/2667-create-hello-world-function.ts
@@ -0,0 +1,11 @@
+// Accepted 25 / 25 testcases passed Sergey Pomortsev submitted at Feb 23, 2026 11:20
+// Solution Runtime 35 ms Beats 89.97% Analyze Complexity Memory 56.10 MB Beats 8.71%
+
+function createHelloWorld() {
+  return () => "Hello World"
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
