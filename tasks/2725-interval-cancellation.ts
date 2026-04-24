bfa8100db5e4ad911ba380b2ef43f2942cbcf58f
Sergey Pomortsev <zaooooza@gmail.com>
2026-04-24 10:31:36 +0500

    added solution for problem 2725 (easy) https://leetcode.com/problems/interval-cancellation

---
+++ tasks/2725-interval-cancellation.ts
@@ -0,0 +1,45 @@
+// Accepted 23 / 23 testcases passed Sergey Pomortsev submitted at Feb 17, 2026 23:10
+// Solution Runtime 47 ms Beats 94.10% Analyze Complexity Memory 55.07 MB Beats 80.26% Analyze Complexity
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+  fn(...args)
+  const interval = setInterval(() => {
+    fn(...args)
+  }, t)
+
+  return () => {
+    clearInterval(interval)
+  }
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [
+ *                           //     {"time":0,"returned":8},
+ *                           //     {"time":35,"returned":8},
+ *                           //     {"time":70,"returned":8},
+ *                           //     {"time":105,"returned":8},
+ *                           //     {"time":140,"returned":8},
+ *                           //     {"time":175,"returned":8}
+ *                           // ]
+ *  }, cancelTimeMs + t + 15)
+ */
