a1eab4f3361e8aebcac157dfb5eb2ac1166d31e4
Sergey Pomortsev <zaooooza@gmail.com>
2026-04-24 10:31:17 +0500

    added solution for problem 2716 (easy) https://leetcode.com/problems/minimize-string-length

---
+++ tasks/2716-minimize-string-length.ts
@@ -0,0 +1,4 @@
+// Accepted 5013 / 5013 testcases passed Sergey Pomortsev submitted at Feb 24, 2026 21:52
+// Solution Runtime 6 ms Beats 100.00% Analyze Complexity Memory 64.91 MB Beats 84.00%
+
+const minimizedStringLength = (s: string): number => new Set(s.split('')).size
