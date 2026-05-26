647f916263cbf11cff9abd7ee36bcc2ef29c53bc
Sergey Pomortsev <zaooooza@gmail.com>
2026-05-26 21:14:42 +0500

    added solution for problem 3925 (easy) https://leetcode.com/problems/concatenate-array-with-reverse

---
+++ tasks/3925-concatenate-array-with-reverse.ts
@@ -0,0 +1,4 @@
+// Accepted 820 / 820 testcases passed  Sergey Pomortsev submitted at May 26, 2026 21:12
+// Solution Runtime 1 ms Beats 90.58% Memory 59.34 MB Beats 94.20%
+
+const concatWithReverse = (nums: number[]): number[] => [...nums, ...nums.reverse()];
