8869e5b1cc7f92352830f424390f22748974577c
Sergey Pomortsev <zaooooza@gmail.com>
2026-08-03 15:48:21 +0500

    added solution for problem 3658 (easy) https://leetcode.com/problems/gcd-of-odd-and-even-sums

---
+++ tasks/3658-gcd-of-odd-and-even-sums.go
@@ -0,0 +1,6 @@
+// Accepted 1000 / 1000 testcases passed Sergey Pomortsev submitted at Jul 15, 2026 21:19
+// Solution Runtime 0 ms Beats 100.00% Memory 3.96 MB Beats 87.50%
+
+func gcdOfOddEvenSums(n int) int {
+  return n
+}
